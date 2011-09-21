cfae19f16191b77b17e7023f586167311a24add5
Reimer Behrends <behrends@gmail.com>
2011-09-21 11:06:01 +0100

    Added missing #include file.

---
+++ src/global.h
@@ -0,0 +1,9 @@
+#ifndef _GAP__GLOBAL_H
+#define _GAP_GLOBAL_H
+
+/* This file contains global #includes for faster compilation */
+
+#include <pthread.h>
+#include <atomic_ops.h>
+
+#endif /* _GAP_GLOBAL_H */
