34e33ee2d1ad61aacc0ab62e76dead5a792f2246
Reimer Behrends <behrends@gmail.com>
2011-11-16 20:23:27 +0100

    Make atomic records handle RecNames() and REC_NAMES() properly.

--- src/aobjects.h
+++ src/aobjects.h
@@ -3,5 +3,6 @@ Obj SetARecordField(Obj record, UInt field, Obj obj);
 Obj GetARecordField(Obj record, UInt field);
 Obj AssTLRecord(Obj record, UInt field, Obj obj);
 Obj GetTLRecordField(Obj record, UInt field);
+Obj FromAtomicRecord(Obj record);
 void SetTLDefault(Obj record, UInt rnam, Obj value);
 void SetTLConstructor(Obj record, UInt rnam, Obj func);
