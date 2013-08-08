52e0ee4d4991ef4f0f950f731207ebbbc0d5ffd4
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2013-08-08 22:56:43 +0100

    Removed from hpcgap branch some outdated directories.
    These were removed from the GAP.dev repository during transition from CVS to Mercurial, so they should not be here either.

--- macsrc/macintr.h
+++
@@ -1,9 +0,0 @@
-
-void ActivateIntr (void);
-
-void ReactivateIntr (void);
-
-extern long syIsIntrFreq;    /* ticks after which to test interrupts    */
-
-extern long syIsIntrTime;    /* next time to test interrupts    */
-
