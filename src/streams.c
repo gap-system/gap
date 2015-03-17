29efa6f556914d60c7b047cc3ab764f4aa37a9c8
Steve Linton <sal@cs.st-andrews.ac.uk>
2015-03-17 17:05:47 +0100

    Change the code that maintains last, last2, last3 to respect Threadlocal variables.

--- src/streams.c
+++ src/streams.c
@@ -277,8 +277,8 @@ static void READ_TEST_OR_LOOP(void)
         if ( type == 0 && TLS->readEvalResult != 0 ) {
 
             /* remember the value in 'last' and the time in 'time'         */
-            AssGVar( Last3, ValGVar( Last2 ) );
-            AssGVar( Last2, ValGVar( Last  ) );
+            AssGVar( Last3, ValGVarTL( Last2 ) );
+            AssGVar( Last2, ValGVarTL( Last  ) );
             AssGVar( Last,  TLS->readEvalResult   );
 
             /* print the result                                            */
