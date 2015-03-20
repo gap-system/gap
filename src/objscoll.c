b32307b579dca4230dfb43822c0a052771e2b2d5
Max Horn <max@quendi.de>
2015-03-20 10:05:24 +0100

    pc collector: reduce differences between 8/16/32 bit collector variants

--- src/objscoll.c
+++ src/objscoll.c
@@ -87,6 +87,8 @@ typedef struct {
 *F * * * * * * * * * * * internal collector functions * * * * * * * * * * * *
 */
 
+#define CB(x)       C8Bits_ ## x
+#define UIntN       UInt1
 #include "src/objscoll-8bit.h"
 
 /****************************************************************************
@@ -100,6 +102,9 @@ FinPowConjCol C8Bits_SingleCollector = {
     C8Bits_Solution
 };
 
+
+#define CB(x)       C16Bits_ ## x
+#define UIntN       UInt2
 #include "src/objscoll-16bit.h"
 
 /****************************************************************************
@@ -113,6 +118,9 @@ FinPowConjCol C16Bits_SingleCollector = {
     C16Bits_Solution
 };
 
+
+#define CB(x)       C32Bits_ ## x
+#define UIntN       UInt4
 #include "src/objscoll-32bit.h"
 
 /****************************************************************************
