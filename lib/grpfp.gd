fa5b36015bb010e756499bf13915cac58fe1769e
Chris Jefferson <caj21@st-andrews.ac.uk>
2015-09-21 17:46:41 +0200

    Third simple library merge

--- lib/grpfp.gd
+++ lib/grpfp.gd
@@ -40,6 +40,7 @@
 ##  <#/GAPDoc>
 ##
 CosetTableDefaultLimit := 1000;
+MakeThreadLocal("CosetTableDefaultLimit");
 
 
 #############################################################################
@@ -128,7 +129,9 @@ CosetTableDefaultLimit := 1000;
 ##  </ManSection>
 ##  <#/GAPDoc>
 ##
-CosetTableDefaultMaxLimit := 2^12*1000;
+
+CosetTableDefaultMaxLimit := 2^12*CosetTableDefaultLimit;
+MakeThreadLocal("CosetTableDefaultMaxLimit");
 
 
 #############################################################################
@@ -153,8 +156,10 @@ CosetTableDefaultMaxLimit := 2^12*1000;
 ##  </ManSection>
 ##  <#/GAPDoc>
 ##
-CosetTableStandard := "lenlex";
 
+MakeThreadLocal("CosetTableStandard");
+BindThreadLocal("CosetTableStandard",MakeImmutable("lenlex"));
+CosetTableStandard := MakeImmutable("lenlex");
 
 #############################################################################
 ##
@@ -224,11 +229,6 @@ InstallTrueMethod(CanEasilyTestMembership, IsSubgroupFgGroup and IsWholeFamily);
 ##  <P/>
 ##  Free groups are a special case of finitely presented groups,
 ##  namely finitely presented groups with no relators.
-##  <P/>
-##  Another special case are groups given by polycyclic presentations.
-##  &GAP; uses a special representation for these groups which is created
-##  in a different way.
-##  See chapter <Ref Chap="Pc Groups"/> for details.
 ##  <Example><![CDATA[
 ##  gap> g:=FreeGroup(2);
 ##  <free group on the generators [ f1, f2 ]>
@@ -239,6 +239,11 @@ InstallTrueMethod(CanEasilyTestMembership, IsSubgroupFgGroup and IsWholeFamily);
 ##  gap> IsFpGroup(h);
 ##  false
 ##  ]]></Example>
+##  <P/>
+##  Another special case are groups given by polycyclic presentations.
+##  &GAP; uses a special representation for these groups which is created
+##  in a different way.
+##  See chapter <Ref Chap="Pc Groups"/> for details.
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
