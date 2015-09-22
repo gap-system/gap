885ac6a2bbd567acd4ff477f1249ce35bf7109fa
Max Horn <max@quendi.de>
2015-09-22 10:00:01 +0200

    src: Add some placeholder headers
    These are empty, but allow us to remove diffs with HPC-GAP.

---
+++ src/atomic.h
@@ -0,0 +1,10 @@
+#ifndef GAP_ATOMIC_H
+#define GAP_ATOMIC_H
+
+/*
+ * This header is a placeholder for the HPC-GAP header of the same name. It
+ * is here to allow us to reduce diffs between the code bases of HPC-GAP
+ * and classic GAP.
+ */
+
+#endif
