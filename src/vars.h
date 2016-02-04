01244e724b68f9cd0b22b62f2ab8f57c3eea4871
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2016-02-04 10:44:11 +0000

    Remove variables from vars.{c,h}

--- src/vars.h
+++ src/vars.h
@@ -41,7 +41,7 @@
 **  'CHANGED_BAG' for  each of such change.  Instead we wait until  a garbage
 **  collection begins  and then  call  'CHANGED_BAG'  in  'BeginCollectBags'.
 */
-extern  Bag             CurrLVars;
+/* TL: extern  Bag             CurrLVars; */
 
 
 
@@ -57,7 +57,7 @@ extern  Bag             CurrLVars;
 **  have to check for the bottom, slowing it down.
 **
 */
-extern  Bag             BottomLVars;
+/* TL: extern  Bag             BottomLVars; */
 
 
 /****************************************************************************
@@ -70,7 +70,7 @@ extern  Bag             BottomLVars;
 **  Since   a   garbage collection may  move   this  bag  around, the pointer
 **  'PtrLVars' must be recalculated afterwards in 'VarsAfterCollectBags'.
 */
-extern  Obj *           PtrLVars;
+/* TL: extern  Obj *           PtrLVars; */
 
 
 /****************************************************************************
