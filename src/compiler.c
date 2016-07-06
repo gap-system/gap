342c6531db8566e7bdf1aa54540d7e3bea4c731d
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2016-07-06 20:03:23 +0100

    Fix compiler for varargs

--- src/compiler.c
+++ src/compiler.c
@@ -752,7 +752,10 @@ void            Emit (
     if ( CompPass != 2 )  return;
 
     /* get the information bag                                             */
-    narg = (NARG_FUNC( CURR_FUNC ) != -1 ? NARG_FUNC( CURR_FUNC ) : 1);
+    narg = NARG_FUNC( CURR_FUNC );
+    if (narg < 0) {
+        narg = -narg;
+    }
 
     /* loop over the format string                                         */
     va_start( ap, fmt );
@@ -5492,7 +5495,10 @@ void CompFunc (
     Int                 i;              /* loop variable                   */
 
     /* get the number of arguments and locals                              */
-    narg = (NARG_FUNC(func) != -1 ? NARG_FUNC(func) : 1);
+    narg = NARG_FUNC(func);
+    if (narg < 0) {
+        narg = -narg;
+    }
     nloc = NLOC_FUNC(func);
 
     /* in the first pass allocate the info bag                             */
