gap-system/gap

commit 3c06dbd8e6298cc42a2cd8655f393a97bf6fbd88

Do not interpret empty lists as strings when Info prints

lib/info.gi
@@ -78,11 +78,11 @@ InstallGlobalFunction( "DefaultInfoHandler", function( infoclass, level, list )
   if out = "*Print*" then
     if IsBoundGlobal( "PrintFormattedString" ) then
       fun := function(s)
-        if IsString(s) and 
-          #XXX this is a temporary hack, we would need a 
+        if (IsString(s) and Length(s) > 0) or IsStringRep(s) and
+          #XXX this is a temporary hack, we would need a
           # IsInstalledGlobal instead of IsBoundGlobal here
                  NARG_FUNC(ValueGlobal("PrintFormattedString")) <> -1 then
-          ValueGlobal( "PrintFormattedString" )(s); 
+          ValueGlobal( "PrintFormattedString" )(s);
         else
           Print(s);
         fi;

commit bce8c876567f56c1ff070c8d7419dd430c7af87b

Fix DefaultInfoHandler
This fixes a problem reported to the GAP Forum on 22 Jul 2016,
namely GAP runs into a breakloop on startup when setting:
  SetUserPreference( "InfoPackageLoadingLevel", 4 );

(The bug was introduced with commit 3c06dbd8.)

lib/info.gi
@@ -78,7 +78,7 @@ InstallGlobalFunction( "DefaultInfoHandler", function( infoclass, level, list )
   if out = "*Print*" then
     if IsBoundGlobal( "PrintFormattedString" ) then
       fun := function(s)
-        if (IsString(s) and Length(s) > 0) or IsStringRep(s) and
+        if (IsString(s) and Length(s) > 0 or IsStringRep(s)) and
           #XXX this is a temporary hack, we would need a
           # IsInstalledGlobal instead of IsBoundGlobal here
                  NARG_FUNC(ValueGlobal("PrintFormattedString")) <> -1 then