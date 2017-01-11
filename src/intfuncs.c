8d768f41394138f842f690133fcb460ac35ddcd3
Max Horn <max@quendi.de>
2017-01-11 19:38:51 +0100

    kernel: cleanup murmur hash code
    Remove the unused function MurmurHash3_x86_128, and also
    rearrange the code so that the 32bit and 64bit versions of
    murmur are separate.

--- src/intfuncs.c
+++ src/intfuncs.c
@@ -241,12 +241,12 @@ integrate with GAP  SL*/
 #include <stdint.h>
 #endif
 
+#define FORCE_INLINE static inline
+
+#ifndef SYS_IS_64_BIT
 
 //-----------------------------------------------------------------------------
 // Platform-specific functions and macros
-
-
-#define FORCE_INLINE static inline
 
 static inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
@@ -254,29 +254,11 @@ static inline uint32_t rotl32 ( uint32_t x, int8_t r )
 }
 #define ROTL32(x,y)     rotl32(x,y)
 
-
-static inline uint64_t rotl64 ( uint64_t x, int8_t r )
-{
-  return (x << r) | (x >> (64 - r));
-}
-
-#define ROTL64(x,y)     rotl64(x,y)
-
-
-
-#define BIG_CONSTANT(x) (x##LLU)
-
-
 //-----------------------------------------------------------------------------
 // Block read - if your platform needs to do endian-swapping or can only
 // handle aligned reads, do the conversion here
 
 FORCE_INLINE uint32_t getblock4 ( const uint32_t * p, int i )
-{
-  return p[i];
-}
-
-FORCE_INLINE uint64_t getblock8 ( const uint64_t * p, int i )
 {
   return p[i];
 }
@@ -293,19 +275,6 @@ FORCE_INLINE uint32_t fmix4 ( uint32_t h )
   h ^= h >> 16;
 
   return h;
-}
-
-//----------
-
-FORCE_INLINE uint64_t fmix8 ( uint64_t k )
-{
-  k ^= k >> 33;
-  k *= BIG_CONSTANT(0xff51afd7ed558ccd);
-  k ^= k >> 33;
-  k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
-  k ^= k >> 33;
-
-  return k;
 }
 
 //-----------------------------------------------------------------------------
@@ -365,113 +334,44 @@ void MurmurHash3_x86_32 ( const void * key, int len,
   *(uint32_t*)out = h1;
 } 
 
+#else
+
 //-----------------------------------------------------------------------------
+// Platform-specific functions and macros
 
-void MurmurHash3_x86_128 ( const void * key, const int len,
-                           uint32_t seed, void * out )
+static inline uint64_t rotl64 ( uint64_t x, int8_t r )
 {
-  const uint8_t * data = (const uint8_t*)key;
-  const int nblocks = len / 16;
+  return (x << r) | (x >> (64 - r));
+}
 
-  uint32_t h1 = seed;
-  uint32_t h2 = seed;
-  uint32_t h3 = seed;
-  uint32_t h4 = seed;
+#define ROTL64(x,y)     rotl64(x,y)
 
-  uint32_t c1 = 0x239b961b; 
-  uint32_t c2 = 0xab0e9789;
-  uint32_t c3 = 0x38b34ae5; 
-  uint32_t c4 = 0xa1e38b93;
 
-  //----------
-  // body
+#define BIG_CONSTANT(x) (x##LLU)
 
-  const uint32_t * blocks = (const uint32_t *)(data + nblocks*16);
 
-  int i;
-  for(i = -nblocks; i; i++)
-  {
-    uint32_t k1 = getblock4(blocks,i*4+0);
-    uint32_t k2 = getblock4(blocks,i*4+1);
-    uint32_t k3 = getblock4(blocks,i*4+2);
-    uint32_t k4 = getblock4(blocks,i*4+3);
+//-----------------------------------------------------------------------------
+// Block read - if your platform needs to do endian-swapping or can only
+// handle aligned reads, do the conversion here
 
-    k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
-
-    h1 = ROTL32(h1,19); h1 += h2; h1 = h1*5+0x561ccd1b;
-
-    k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
-
-    h2 = ROTL32(h2,17); h2 += h3; h2 = h2*5+0x0bcaa747;
-
-    k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
-
-    h3 = ROTL32(h3,15); h3 += h4; h3 = h3*5+0x96cd1c35;
-
-    k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
-
-    h4 = ROTL32(h4,13); h4 += h1; h4 = h4*5+0x32ac3b17;
-  }
-
-  //----------
-  // tail
-
-  const uint8_t * tail = (const uint8_t*)(data + nblocks*16);
-
-  uint32_t k1 = 0;
-  uint32_t k2 = 0;
-  uint32_t k3 = 0;
-  uint32_t k4 = 0;
-
-  switch(len & 15)
-  {
-  case 15: k4 ^= tail[14] << 16;
-  case 14: k4 ^= tail[13] << 8;
-  case 13: k4 ^= tail[12] << 0;
-           k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
-
-  case 12: k3 ^= tail[11] << 24;
-  case 11: k3 ^= tail[10] << 16;
-  case 10: k3 ^= tail[ 9] << 8;
-  case  9: k3 ^= tail[ 8] << 0;
-           k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
-
-  case  8: k2 ^= tail[ 7] << 24;
-  case  7: k2 ^= tail[ 6] << 16;
-  case  6: k2 ^= tail[ 5] << 8;
-  case  5: k2 ^= tail[ 4] << 0;
-           k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
-
-  case  4: k1 ^= tail[ 3] << 24;
-  case  3: k1 ^= tail[ 2] << 16;
-  case  2: k1 ^= tail[ 1] << 8;
-  case  1: k1 ^= tail[ 0] << 0;
-           k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
-  };
-
-  //----------
-  // finalization
-
-  h1 ^= len; h2 ^= len; h3 ^= len; h4 ^= len;
-
-  h1 += h2; h1 += h3; h1 += h4;
-  h2 += h1; h3 += h1; h4 += h1;
-
-  h1 = fmix4(h1);
-  h2 = fmix4(h2);
-  h3 = fmix4(h3);
-  h4 = fmix4(h4);
-
-  h1 += h2; h1 += h3; h1 += h4;
-  h2 += h1; h3 += h1; h4 += h1;
-
-  ((uint32_t*)out)[0] = h1;
-  ((uint32_t*)out)[1] = h2;
-  ((uint32_t*)out)[2] = h3;
-  ((uint32_t*)out)[3] = h4;
+FORCE_INLINE uint64_t getblock8 ( const uint64_t * p, int i )
+{
+  return p[i];
 }
 
 //-----------------------------------------------------------------------------
+// Finalization mix - force all bits of a hash block to avalanche
+
+FORCE_INLINE uint64_t fmix8 ( uint64_t k )
+{
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+  k ^= k >> 33;
+
+  return k;
+}
 
 void MurmurHash3_x64_128 ( const void * key, const int len,
                            const UInt4 seed, void * out )
@@ -552,7 +452,7 @@ void MurmurHash3_x64_128 ( const void * key, const int len,
   ((uint64_t*)out)[0] = h1;
   ((uint64_t*)out)[1] = h2;
 }
-
+#endif
 
 Obj FuncHASHKEY_BAG(Obj self, Obj obj, Obj opSeed, Obj opOffset, Obj opMaxLen)
 {
