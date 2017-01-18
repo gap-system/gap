9d2f2b99f5e5f737eaf3ced89087917ee95387e6
Chris Jefferson <caj21@st-andrews.ac.uk>
2017-01-18 09:41:03 +0000

    Regenerate error tests using '-r'

--- tst/test-error/run_error_tests.sh
+++ tst/test-error/run_error_tests.sh
@@ -2,7 +2,7 @@
 
 retvalue=0
 for gfile in *.g; do
-    if ! diff "${gfile}.out" <(./run_gap.sh "../../bin/gap.sh -r -A" "${gfile}"); then
+    if ! diff "${gfile}.out" <(./run_gap.sh "../../bin/gap.sh" "${gfile}"); then
         echo "${gfile}" failed
         retvalue=1
     fi;
