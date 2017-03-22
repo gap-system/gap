97560909408bc3d12b25dba8d7f721f445564ff4
Max Horn <max@quendi.de>
2017-03-22 16:54:03 +0100

    build: add libtool files to repository

---
+++ aclocal.m4
@@ -0,0 +1,9 @@
+# Make sure to include any M4 files needed by configure.ac here
+m4_include([cnf/m4/ax_check_library.m4])
+m4_include([cnf/m4/ax_pthread.m4])
+m4_include([cnf/m4/gap.m4])
+m4_include([cnf/m4/libtool.m4])
+m4_include([cnf/m4/ltoptions.m4])
+m4_include([cnf/m4/ltsugar.m4])
+m4_include([cnf/m4/ltversion.m4])
+m4_include([cnf/m4/lt~obsolete.m4])
--- autogen.sh
+++ autogen.sh
@@ -2,12 +2,4 @@
 #
 # Regenerate configure from configure.ac. Requires GNU autoconf.
 
-# Workaround problem caused by running this script with an outdated
-# version of libtool (i.e. before 2.4.2), which then causes subsequent
-# runs with a current version to fail.
-rm -rf aclocal.m4 autom4te.cache cnf/ltmain.sh cnf/m4/l*.m4
-
-# We use --no-recursive to prevent autoreconf from running itself in
-# extern/gmp.  And we use '-I cnf/m4' as it helps with some older
-# autoconf versions.
-autoreconf -vif --no-recursive -I cnf/m4 `dirname "$0"`
+autoconf
