928deb44080a0f6ea185d7b0351b779904d38ee1
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-03-24 17:22:04 +0100

    hpcgap: merge timer changes from master into hpcgap

--- hpcgap/src/sysfiles.h
+++ hpcgap/src/sysfiles.h
@@ -349,6 +349,7 @@ extern int SyHaveAlarms;
 extern volatile int SyAlarmRunning;
 extern volatile int SyAlarmHasGoneOff;
 
+extern void SyInitAlarm( void );
 extern void SyInstallAlarm( UInt seconds, UInt nanoseconds);
 extern void SyStopAlarm( UInt *seconds, UInt *nanoseconds);
 
