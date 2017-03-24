634bd8114777ba67a3f5c2859e861696040ec6e7
Max Horn <max@quendi.de>
2017-03-24 17:22:04 +0100

    etc/ci-prepare.sh: new script for building GAP on CI servers
    * Takes care of HPCGAP support (just set env var HPCGAP to yes),
      including ward setup
    * Build GAP with V=1 for easier debugging of build failures on CI server
    * Supports out-of-tree builds

---
+++ etc/ci-prepare.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -ex
+
+SRCDIR=${SRCDIR:-$PWD}
+
+# prepare the build system
+cd $SRCDIR
+./autogen.sh
+
+# change into BUILDDIR (creating it if necessary), and turn it into an absolute path
+if [[ -n "$BUILDDIR" ]]
+then
+  mkdir -p "$BUILDDIR"
+  cd "$BUILDDIR"
+fi
+BUILDDIR=$PWD
+
+# for HPC-GAP we install ward inside BUILDDIR
+if [[ $HPCGAP = yes ]]
+then
+  git clone https://github.com/gap-system/ward
+  cd ward
+  CFLAGS= LDFLAGS= ./build.sh
+  cd ..
+  CONFIGFLAGS="$CONFIGFLAGS --enable-hpcgap"
+fi
+
+# configure and make GAP
+"$SRCDIR/configure" $CONFIGFLAGS
+make V=1 -j4
+
+# check that GAP is at least apple to start
+echo 'Print("GAP started successfully\n");QUIT_GAP(0);' | ./gap -q -T
+
+# download packages
+make bootstrap-pkg-full
+
+# packages must be placed inside SRCDIR, as only that
+# is a GAP root, while BUILDDIR is not.
+if [[ ! -d "$SRCDIR/pkg" ]]
+then
+  mv pkg "$SRCDIR/"
+fi
