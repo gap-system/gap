36e339e8430c173908f96cf79beeff495f6d14d4
Max Horn <max@quendi.de>
2017-04-10 10:43:21 +0200

    src/gasman.h: fix comment typo: InitGlobalBags -> InitGlobalBag

--- src/gasman.h
+++ src/gasman.h
@@ -958,7 +958,7 @@ extern TNumGlobalBags GlobalBags;
 **  problem  if  such a  variable does not   actually  hold a bag identifier,
 **  {\Gasman} will simply ignore it then.
 **
-**  There is a limit on the number of calls to 'InitGlobalBags', which is 512
+**  There is a limit on the number of calls to 'InitGlobalBag', which is 512
 **  by default.   If the application has  more global variables that may hold
 **  bag  identifier, you  have to  compile  {\Gasman} with a  higher value of
 **  'NR_GLOBAL_BAGS', i.e., with 'make COPTS=-DNR_GLOBAL_BAGS=<nr>'.
