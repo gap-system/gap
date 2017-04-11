47c3049f9a73b2233eeb8edfd0ff0c534a04e119
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-04-11 16:05:13 +0100

    Rename DestroyGlobal to DestroyGAPState

--- src/gapstate.c
+++ src/gapstate.c
@@ -39,7 +39,7 @@ void InitGAPState(GAPState *state)
     // RunConstructors?
 }
 
-void DestroyGlobal(GAPState *state)
+void DestroyGAPState(GAPState *state)
 {
     DestroyScannerState(state);
     DestroyStatState(state);
