7ed6a899ae8a8905d00fa6ef143a6f92f3d26c8a
Max Horn <max@quendi.de>
2017-04-18 10:27:28 +0200

    hpcgap/src/system.{c,h}: migrate SyUSleep to GAP

--- src/system.c
+++ src/system.c
@@ -1279,6 +1279,18 @@ void SySleep ( UInt secs )
 
 /****************************************************************************
 **
+*F  SyUSleep( <msecs> ) . . . . . . . . . .sleep GAP for <msecs> microseconds
+**
+**  NB Various OS events (like signals) might wake us up
+**
+*/
+void SyUSleep ( UInt msecs )
+{
+  usleep( (unsigned int) msecs );
+}
+
+/****************************************************************************
+**
 
 *F * * * * * * * * * * * * * initialize package * * * * * * * * * * * * * * *
 */
