5169a2e613105cd4e1eb6553915a47c0adbd1a7a
Max Horn <max@quendi.de>
2017-04-24 15:17:02 +0200

    etc/ffgen.c: tweak generated code

--- src/ffdata.h
+++ src/ffdata.h
@@ -1,7 +1,9 @@
-#ifndef _GAP_FFDATA_H
-#define _GAP_FFDATA_H
+#ifndef GAP_FFDATA_H
+#define GAP_FFDATA_H
 
-#define NUM_SHORT_FINITE_FIELDS 6635
+enum {
+    NUM_SHORT_FINITE_FIELDS = 6635
+};
 
 extern unsigned long SizeFF[NUM_SHORT_FINITE_FIELDS+1];
 extern unsigned char DegrFF[NUM_SHORT_FINITE_FIELDS+1];
