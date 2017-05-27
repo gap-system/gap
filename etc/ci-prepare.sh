7a91f7e9fa6f7695b0927c7c7378be72529d9c94
Chris Jefferson <caj21@st-andrews.ac.uk>
2017-05-27 07:42:44 +0100

    Delete factint in HPC-GAP, as the package breaks factorisation

--- etc/ci-prepare.sh
+++ etc/ci-prepare.sh
@@ -37,9 +37,17 @@ echo 'Print("GAP started successfully\n");QUIT_GAP(0);' | ./gap -q -T
 # connection is refused, to work around intermittent failures
 make bootstrap-pkg-full WGET="wget -N --no-check-certificate --tries=5 --waitretry=5 --retry-connrefused"
 
+# TEMPORARY FIX : factint is not HPC-GAP compatible
+if [[ $HPCGAP = yes ]]
+then
+    rm -rf pkg/factint*
+fi;
+
+
 # packages must be placed inside SRCDIR, as only that
 # is a GAP root, while BUILDDIR is not.
 if [[ ! -d "$SRCDIR/pkg" ]]
 then
   mv pkg "$SRCDIR/"
 fi
+
