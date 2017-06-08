61e0fb8279d90e35ad507fe1c8c313b02255392b
Max Horn <max@quendi.de>
2017-06-08 12:16:28 +0200

    hpcgap: merge lib/vec8bit.gd

--- hpcgap/lib/vec8bit.gd
+++ hpcgap/lib/vec8bit.gd
@@ -15,10 +15,11 @@
 ##
 #V  PRIMES_COMPACT_FIELDS    primes for which a compact representation exists
 ##
-BIND_GLOBAL("PRIMES_COMPACT_FIELDS", `SSortedList(
+BIND_GLOBAL("PRIMES_COMPACT_FIELDS",SSortedList(
   [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,
   101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,
   193,197,199,211,223,227,229,233,239,241,251] ));
+MakeImmutable(PRIMES_COMPACT_FIELDS);
 
 #############################################################################
 ##
