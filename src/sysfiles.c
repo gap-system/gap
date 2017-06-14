8d0cb03840aa7aaf864e97e6fc1479b3dce2ce79
Max Horn <max@quendi.de>
2017-06-14 16:45:08 +0200

    kernel: make syCcitt32 array const

--- src/sysfiles.c
+++ src/sysfiles.c
@@ -190,7 +190,7 @@ Int SyFindOrLinkGapRootFile (
 **
 **  This function *never* returns a 0 unless an error occurred.
 */
-static UInt4 syCcitt32[ 256 ] =
+static const UInt4 syCcitt32[ 256 ] =
 {
 0x00000000L, 0x77073096L, 0xee0e612cL, 0x990951baL, 0x076dc419L,
 0x706af48fL, 0xe963a535L, 0x9e6495a3L, 0x0edb8832L, 0x79dcb8a4L, 0xe0d5e91eL,
