b7afc33ad7a0d1192762bfc30631db9821e98a80
Max Horn <max@quendi.de>
2017-07-12 11:12:46 +0200

    Hotfix for master: fix DeclareObsoleteSynonym call

--- lib/obsolete.gd
+++ lib/obsolete.gd
@@ -560,7 +560,7 @@ DeclareObsoleteSynonym( "USER_HOME_EXPAND", "UserHomeExpand" );
 ##  This name stems from GAP 3 days.
 ##
 ## still used by Browse, ctbllib, gapdoc, genss, io, orb (05/2017)
-DeclareObsoleteSynonym( "RecFields", "RecNames", "4.0" );
+DeclareObsoleteSynonym( "RecFields", "RecNames" );
 
 #############################################################################
 ##
