2a5e137425a15978f4d9d603853f2d5d3084088d
Max Horn <max@quendi.de>
2017-08-21 18:33:56 +0200

    Skip help tests

--- etc/ci-packages.sh
+++ etc/ci-packages.sh
@@ -6,7 +6,8 @@ packages=$($GAP -A <<GAPInput
     packages := SortedList(ShallowCopy(RecNames(GAPInfo.PackagesInfo)));;
     # Remove some packages
     SubtractSet(packages, [
-        "cubefree", # test take too long
+        "cubefree", # tests take too long
+        "help",     # tests take too long
     ]);
     Perform(packages,Display);
     QUIT_GAP(0);
