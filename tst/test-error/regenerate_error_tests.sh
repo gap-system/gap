20166bfa699c012542ada553e3c8db2014889431
Max Horn <max@quendi.de>
2017-09-13 23:21:05 +0200

    tst/test-error: factor out path to gap executable

--- tst/test-error/regenerate_error_tests.sh
+++ tst/test-error/regenerate_error_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+gap="../../bin/gap.sh"
 for gfile in *.g; do
-    ./run_gap.sh "../../bin/gap.sh" "${gfile}" > "${gfile}.out"
+    ./run_gap.sh "${gap}" "${gfile}" > "${gfile}.out"
 done
-
