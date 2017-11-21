251b4d895959cb8a1cac7a5a413713a3ddae5044
Max Horn <max@quendi.de>
2017-11-21 16:32:37 +0100

    Change MODULE_STATE name from 'funcs' to 'Funcs'
    We use capitalized identifiers for all other global variables and global
    typedefs, so it seems weird to make this one an exception

--- src/funcs.c
+++ src/funcs.c
@@ -57,20 +57,20 @@
 
 #include <src/hookintrprtr.h>
 
-static ModuleStateOffset funcsStateOffset;
+static ModuleStateOffset FuncsStateOffset;
 typedef struct {
     Int RecursionDepth;
     Obj ExecState;
-} funcsModuleState;
+} FuncsModuleState;
 
 void IncRecursionDepth(void)
 {
-    MODULE_STATE(funcs, RecursionDepth)++;
+    MODULE_STATE(Funcs, RecursionDepth)++;
 }
 
 void DecRecursionDepth(void)
 {
-    MODULE_STATE(funcs, RecursionDepth)--;
+    MODULE_STATE(Funcs, RecursionDepth)--;
     /* FIXME: According to a comment in the function
               RecursionDepthTrap below, RecursionDepth
               can become "slightly" negative. This
@@ -81,13 +81,13 @@ void DecRecursionDepth(void)
 
 Int GetRecursionDepth(void)
 {
-    return MODULE_STATE(funcs, RecursionDepth);
+    return MODULE_STATE(Funcs, RecursionDepth);
 }
 
 void SetRecursionDepth(Int depth)
 {
     GAP_ASSERT(depth >= 0);
-    MODULE_STATE(funcs, RecursionDepth) = depth;
+    MODULE_STATE(Funcs, RecursionDepth) = depth;
 }
 
 /****************************************************************************
@@ -1707,12 +1707,12 @@ void            ExecBegin ( Obj frame )
     /* remember the old execution state                                    */
     execState = NEW_PLIST(T_PLIST, 3);
     SET_LEN_PLIST(execState, 3);
-    SET_ELM_PLIST(execState, 1, MODULE_STATE(funcs, ExecState));
+    SET_ELM_PLIST(execState, 1, MODULE_STATE(Funcs, ExecState));
     SET_ELM_PLIST(execState, 2, STATE(CurrLVars));
     /* the 'CHANGED_BAG(STATE(CurrLVars))' is needed because it is delayed        */
     CHANGED_BAG( STATE(CurrLVars) );
     SET_ELM_PLIST(execState, 3, INTOBJ_INT((Int)STATE(CurrStat)));
-    MODULE_STATE(funcs, ExecState) = execState;
+    MODULE_STATE(Funcs, ExecState) = execState;
 
     /* set up new state                                                    */
     SWITCH_TO_OLD_LVARS( frame );
@@ -1731,9 +1731,9 @@ void            ExecEnd (
     }
 
     /* switch back to the old state                                    */
-    SET_BRK_CURR_STAT((Stat)INT_INTOBJ(ELM_PLIST(MODULE_STATE(funcs, ExecState), 3)));
-    SWITCH_TO_OLD_LVARS( ELM_PLIST(MODULE_STATE(funcs, ExecState), 2) );
-    MODULE_STATE(funcs, ExecState) = ELM_PLIST(MODULE_STATE(funcs, ExecState), 1);
+    SET_BRK_CURR_STAT((Stat)INT_INTOBJ(ELM_PLIST(MODULE_STATE(Funcs, ExecState), 3)));
+    SWITCH_TO_OLD_LVARS( ELM_PLIST(MODULE_STATE(Funcs, ExecState), 2) );
+    MODULE_STATE(Funcs, ExecState) = ELM_PLIST(MODULE_STATE(Funcs, ExecState), 1);
 }
 
 /****************************************************************************
@@ -1806,7 +1806,7 @@ static Int InitKernel (
 
 #if !defined(HPCGAP)
     /* make the global variable known to Gasman                            */
-    InitGlobalBag( &MODULE_STATE(funcs, ExecState), "src/funcs.c:ExecState" );
+    InitGlobalBag( &MODULE_STATE(Funcs, ExecState), "src/funcs.c:ExecState" );
 #endif
 
     /* Register the handler for our exported function                      */
@@ -1890,8 +1890,8 @@ static Int InitKernel (
 
 static void InitModuleState(ModuleStateOffset offset)
 {
-    MODULE_STATE(funcs, ExecState) = 0;
-    MODULE_STATE(funcs, RecursionDepth) = 0;
+    MODULE_STATE(Funcs, ExecState) = 0;
+    MODULE_STATE(Funcs, RecursionDepth) = 0;
 }
 
 /****************************************************************************
@@ -1915,6 +1915,6 @@ static StructInitInfo module = {
 
 StructInitInfo * InitInfoFuncs ( void )
 {
-    funcsStateOffset = RegisterModuleState(sizeof(funcsModuleState), InitModuleState, 0);
+    FuncsStateOffset = RegisterModuleState(sizeof(FuncsModuleState), InitModuleState, 0);
     return &module;
 }
