45564d50fb63e32a9c14ff251b52a7151d3eb5d1
Max Horn <max@quendi.de>
2017-12-02 14:19:21 +0100

    tests: tweak scripts in tst/test-{compile,error}/ some more

--- tst/test-compile/run_single_test.sh
+++ tst/test-compile/run_single_test.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-# This script should be run as ./run_gap.sh gap gac gapfile.g
+# This script should be run as ./run_single_test.sh gap gapfile.g
 # It reads gapfile.g, then runs the function 'runtest'
+gap="$1"
+gfile="$2"
 
 # It provides the following features:
 # 1) Stop GAP from attaching to the terminal (which it will
@@ -12,4 +14,6 @@ set -e
 # 3) Rewrite the root of gap with the string GAPROOT,
 #    so the output is usable on other machines
 GAPROOT=$(cd ../..; pwd)
-echo 'Read("'$2'"); runtest();' | $1 -r -A -q -b -x 200 2>&1 | sed "s:${GAPROOT//:/\\:}:GAPROOT:g"
+echo "Read(\"$gfile\"); runtest();" |
+    "$gap" -r -A -q -b -x 200 2>&1 |
+    sed "s:${GAPROOT//:/\\:}:GAPROOT:g"
--- tst/test-error/run_gap.sh
+++ tst/test-error/run_gap.sh
@@ -3,6 +3,9 @@
 set -e
 
 # This script should be run as ./run_gap.sh gap gapfile.g
+gap="$1"
+gfile="$2"
+
 # It provides the following features:
 # 1) Stop GAP from attaching to the terminal (which it will
 #    use in the break loop)
@@ -10,4 +13,6 @@ set -e
 # 3) Rewrite the root of gap with the string GAPROOT,
 #    so the output is usable on other machines
 GAPROOT=$(cd ../..; pwd)
-cat "$2" | "$1" -r -A -q -b -x 200 2>&1 | sed "s:${GAPROOT//:/\\:}:GAPROOT:g"
+cat "$gfile" |
+    "$gap" -r -A -q -b -x 200 2>&1 |
+    sed "s:${GAPROOT//:/\\:}:GAPROOT:g"
