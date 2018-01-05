7d05a666a69b1a76fb25bc8fb8ce88fc0c780ee5
Max Horn <max@quendi.de>
2018-01-05 11:39:55 +0100

    kernel: use TNAM_OBJ where possible

--- src/macfloat.c
+++ src/macfloat.c
@@ -292,7 +292,7 @@ Obj FuncMACFLOAT_STRING( Obj self, Obj s )
   while (!IsStringConv(s))
     {
       s = ErrorReturnObj("MACFLOAT_STRING: object to be converted must be a string not a %s",
-			 (Int)(InfoBags[TNUM_OBJ(s)].name),0,"You can return a string to continue" );
+			 (Int)TNAM_OBJ(s),0,"You can return a string to continue" );
     }
   char * endptr;
   UChar *sp = CHARS_STRING(s);
