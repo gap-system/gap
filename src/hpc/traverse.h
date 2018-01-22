92977b0b310faf6fa7523844501a7f3db2b7f240
Max Horn <max@quendi.de>
2018-01-22 16:43:41 +0100

    Refactor HPC-GAP specific traverse kernel code

--- src/hpc/traverse.h
+++ src/hpc/traverse.h
@@ -8,12 +8,34 @@
  */
 
 typedef void (*TraversalFunction)(Obj);
+typedef void (*TraversalCopyFunction)(Obj copy, Obj original);
 
-extern TraversalFunction TraversalFunc[];
+typedef enum {
+    TRAVERSE_NONE,
+    TRAVERSE_BY_FUNCTION,
+    TRAVERSE_ALL,
+    TRAVERSE_ALL_BUT_FIRST,
+} TraversalMethodEnum;
+
+// set the traversal method (and optionally, helper functions)
+// for all objects with the specified tnum.
+extern void SetTraversalMethod(UInt tnum,
+                               TraversalMethodEnum meth,
+                               TraversalFunction tf,
+                               TraversalCopyFunction cf);
+
+
+// helper to be called from traverse functions
+extern void QueueForTraversal(Obj obj);
+
+// helper to be called from copy functions
+extern Obj ReplaceByCopy(Obj obj);
+
 
 Obj ReachableObjectsFrom(Obj obj);
 Obj CopyReachableObjectsFrom(Obj obj, int delimited, int asList, int imm);
 Obj CopyTraversed(Obj traversed);
 int PreMakeImmutableCheck(Obj obj);
+
 
 #endif // GAP_TRAVERSE_H
