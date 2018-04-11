34d3adb4c2b5a8d3d8f94bd2361bc21e41610c55
Max Horn <max@quendi.de>
2018-04-11 16:05:51 +0200

    ci: print environment vars and info about HEAD commit

--- etc/ci-prepare.sh
+++ etc/ci-prepare.sh
@@ -4,6 +4,10 @@ set -ex
 
 SRCDIR=${SRCDIR:-$PWD}
 
+# for debugging purposes print the environment and info about the HEAD commit
+printenv | sort
+git show --pretty=fuller -s
+
 # prepare the build system
 cd $SRCDIR
 ./autogen.sh
