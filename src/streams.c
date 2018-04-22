b7d4e1cc86085642a9cd34ccb7cdf0760e034ef1
Max Horn <max@quendi.de>
2018-04-22 22:56:48 +0200

    WIP: tabs to spaces

--- src/streams.c
+++ src/streams.c
@@ -222,8 +222,8 @@ static Int READ_INNER ( UInt UseUHQ )
         ClearError();
         Obj evalResult;
         ExecStatus status = ReadEvalCommand(STATE(BottomLVars), &evalResult, 0);
-	if (STATE(UserHasQuit) || STATE(UserHasQUIT))
-	  break;
+        if (STATE(UserHasQuit) || STATE(UserHasQUIT))
+          break;
         /* handle return-value or return-void command                      */
         if ( status & (STATUS_RETURN_VAL | STATUS_RETURN_VOID) ) {
             Pr(
@@ -1702,9 +1702,9 @@ Obj FuncREAD_ALL_FILE (
 
     while ( ! IS_INTOBJ(limit) ) {
       limit = ErrorReturnObj(
-			     "<limit> must be a small integer (not a %s)",
-			     (Int)TNAM_OBJ(limit), 0L,
-			     "you can replace limit via 'return <limit>;'" );
+                             "<limit> must be a small integer (not a %s)",
+                             (Int)TNAM_OBJ(limit), 0L,
+                             "you can replace limit via 'return <limit>;'" );
     }
     ilim = INT_INTOBJ(limit);
 
@@ -1717,55 +1717,55 @@ Obj FuncREAD_ALL_FILE (
 
     if (syBuf[ifid].bufno >= 0)
       {
-	UInt bufno = syBuf[ifid].bufno;
+        UInt bufno = syBuf[ifid].bufno;
 
-	/* first drain the buffer */
-	lstr = syBuffers[bufno].buflen - syBuffers[bufno].bufstart;
-	if (ilim != -1)
-	  {
-	    if (lstr > ilim)
-	      lstr = ilim;
-	    ilim -= lstr;
-	  }
-	GROW_STRING(str, lstr);
-	memcpy(CHARS_STRING(str), syBuffers[bufno].buf + syBuffers[bufno].bufstart, lstr);
-	len = lstr;
-	SET_LEN_STRING(str, len);
-	syBuffers[bufno].bufstart += lstr;
+        /* first drain the buffer */
+        lstr = syBuffers[bufno].buflen - syBuffers[bufno].bufstart;
+        if (ilim != -1)
+          {
+            if (lstr > ilim)
+              lstr = ilim;
+            ilim -= lstr;
+          }
+        GROW_STRING(str, lstr);
+        memcpy(CHARS_STRING(str), syBuffers[bufno].buf + syBuffers[bufno].bufstart, lstr);
+        len = lstr;
+        SET_LEN_STRING(str, len);
+        syBuffers[bufno].bufstart += lstr;
       }
 #ifdef SYS_IS_CYGWIN32
  getmore:
 #endif
     while (ilim == -1 || len < ilim ) {
       if ( len > 0 && !HasAvailableBytes(ifid))
-	break;
+        break;
       if (syBuf[ifid].isTTY)
-	{
-	  if (ilim == -1)
-	    {
-	      Pr("#W Warning -- reading to  end of input tty will never end\n",0,0);
-	      csize = 20000;
-	    }
-	  else
-	      csize = ((ilim- len) > 20000) ? 20000 : ilim - len;
-	    
-	  if (SyFgetsSemiBlock(buf, csize, ifid))
-	    lstr = strlen(buf);
-	  else  
-	    lstr = 0;
-	}
+        {
+          if (ilim == -1)
+            {
+              Pr("#W Warning -- reading to  end of input tty will never end\n",0,0);
+              csize = 20000;
+            }
+          else
+              csize = ((ilim- len) > 20000) ? 20000 : ilim - len;
+            
+          if (SyFgetsSemiBlock(buf, csize, ifid))
+            lstr = strlen(buf);
+          else  
+            lstr = 0;
+        }
       else
-	{
-	  do {
-	    csize = (ilim == -1 || (ilim- len) > 20000) ? 20000 : ilim - len;
-	    lstr = read(syBuf[ifid].fp, buf, csize);
-	  } while (lstr == -1 && errno == EAGAIN);
-	}
+        {
+          do {
+            csize = (ilim == -1 || (ilim- len) > 20000) ? 20000 : ilim - len;
+            lstr = read(syBuf[ifid].fp, buf, csize);
+          } while (lstr == -1 && errno == EAGAIN);
+        }
       if (lstr <= 0)
-	{
-	  syBuf[ifid].ateof = 1;
-	  break;
-	}
+        {
+          syBuf[ifid].ateof = 1;
+          break;
+        }
       GROW_STRING( str, len+lstr );
       memcpy(CHARS_STRING(str)+len, buf, lstr);
       len += lstr;
@@ -1779,23 +1779,23 @@ Obj FuncREAD_ALL_FILE (
     {
       UInt i = 0,j = 0;
       while ( i < len )
-	{
-	  if (CHARS_STRING(str)[i] == '\r')
-	    {
-	      if (i < len -1 && CHARS_STRING(str)[i+1] == '\n')
-		{
-		  i++;
-		  continue;
-		}
-	      else
-		CHARS_STRING(str)[i] = '\n';
-	    }
-	  CHARS_STRING(str)[j++] = CHARS_STRING(str)[i++];
-	}
+        {
+          if (CHARS_STRING(str)[i] == '\r')
+            {
+              if (i < len -1 && CHARS_STRING(str)[i+1] == '\n')
+                {
+                  i++;
+                  continue;
+                }
+              else
+                CHARS_STRING(str)[i] = '\n';
+            }
+          CHARS_STRING(str)[j++] = CHARS_STRING(str)[i++];
+        }
       len = j;
       SET_LEN_STRING(str, len);
       if (ilim != -1 && len < ilim)
-	goto getmore;
+        goto getmore;
       
     }
 #endif
