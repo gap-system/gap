gap-system/gap

commit 51182b2595b732cb294c15b24dc4f4b1136f8e44

Quit interpreter early if first symbol has syntax error
If the first Match in ReadEvalCommand leads to an error
just return.

This fixes issue #995.

src/read.c
@@ -2679,6 +2679,11 @@ ExecStatus ReadEvalCommand ( Obj context, UInt *dualSemicolon )
     /* get the first symbol from the input                                 */
     Match( TLS(Symbol), "", 0UL );
 
+    /* using READ_ERROR sets the jump buffer and mucks up the interpreter */
+    if (TLS(NrError)) {
+        return STATUS_ERROR;
+    }
+
     /* if we have hit <end-of-file>, then give up                          */
     if ( TLS(Symbol) == S_EOF )  { return STATUS_EOF; }
 

commit a9b169e07311aa3739c1517a053a192e4885aeb6

kernel: flush input line after early syntax error
Fixes a mild regression introduce by PR #1015, which in turn attempted
to fix parts of issue #995. With GAP 4.8 and also with this fix:

    gap> 1.2\3;
    Syntax error: Badly formed number
    1.2\3;
       ^

In GAP 4.9:

    gap> 1.2\3;
    Syntax error: Badly formed number
    1.2\3;
       ^
    3

src/read.c
@@ -2713,8 +2713,9 @@ ExecStatus ReadEvalCommand(Obj context, Obj *evalResult, UInt *dualSemicolon)
     /* get the first symbol from the input                                 */
     Match( STATE(Symbol), "", 0UL );
 
-    /* using TRY_READ sets the jump buffer and mucks up the interpreter    */
+    // if scanning the first symbol produced a syntax error, abort
     if (STATE(NrError)) {
+        FlushRestOfInputLine();
         return STATUS_ERROR;
     }
 

tst/testinstall/kernel/read.tst
@@ -65,4 +65,70 @@ rec("a":=1);
       ^
 
 #
+# ReadEvalCommand
+#
+# See also https://github.com/gap-system/gap/issues/995
+#
+
+# some inputs which immediately trigger an error in ReadEvalCommand,
+# even before an interpreter has been started
+gap> '
+Syntax error: Character literal must not include <newline> in stream:1
+'
+^
+gap> 'x
+Syntax error: Missing single quote in character constant in stream:1
+'x
+ ^
+gap> "
+Syntax error: String must not include <newline> in stream:1
+"
+^
+gap> "x
+Syntax error: String must not include <newline> in stream:1
+"x
+ ^
+
+# similar inputs to the above, but here the error is triggered a bit
+# later, after the interpreter has already started
+gap> s := '
+Syntax error: Character literal must not include <newline> in stream:1
+s := '
+     ^
+Syntax error: ; expected in stream:2
+^
+gap> s := 'x
+Syntax error: Missing single quote in character constant in stream:1
+s := 'x
+      ^
+Syntax error: ; expected in stream:2
+^
+gap> s := "
+Syntax error: String must not include <newline> in stream:1
+s := "
+     ^
+Syntax error: ; expected in stream:2
+^
+gap> s := "x
+Syntax error: String must not include <newline> in stream:1
+s := "x
+      ^
+Syntax error: ; expected in stream:2
+^
+
+# errors in the middle of parsing a float literal
+gap> 12.34\56;
+Syntax error: Badly formed number in stream:1
+12.34\56;
+     ^
+gap> 12.34\a56;
+Syntax error: Badly formed number in stream:1
+12.34\a56;
+     ^
+gap> 12.34\56a;
+Syntax error: Badly formed number in stream:1
+12.34\56a;
+     ^
+
+#
 gap> STOP_TEST("kernel/read.tst", 1);