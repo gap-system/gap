6e3fa16afff5fc75a5883f9ff0dde35be435bcce
Chris Jefferson <caj21@st-andrews.ac.uk>
2018-05-13 20:04:11 +0100

    Add checks for outputting FilenameIds
    This was previously not necessary, as we always checked when entering
    and leaving a function, but I want to improve coverage in HPC-GAP to stop
    outputting enter/leave functions (which get mixed up between threads)

--- src/profile.c
+++ src/profile.c
@@ -350,6 +350,7 @@ static inline void outputStat(Stat stat, int exec, int visited)
                             profileState.minimumProfileTick;
             }
             ticks -= ticksDone;
+            outputFilenameIdIfRequired(nameid);
             fprintf(
                 profileState.Stream,
                 "{\"Type\":\"%c\",\"Ticks\":%d,\"Line\":%d,\"FileId\":%d}\n",
@@ -366,6 +367,7 @@ static inline void outputStat(Stat stat, int exec, int visited)
       }
     }
     else {
+      outputFilenameIdIfRequired(nameid);
       fprintf(profileState.Stream, "{\"Type\":\"%c\",\"Line\":%d,\"FileId\":%d}\n",
               exec ? 'E' : 'R', (int)line, (int)nameid);
       profileState.lastOutputted.line = line;
