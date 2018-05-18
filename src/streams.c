79a4ac4e78e27cdab915c21bd96fe50d8162b6a1
Max Horn <max@quendi.de>
2018-05-18 18:12:35 +0200

    kernel: tabs to spaces, code formatting in streams.c

--- src/streams.c
+++ src/streams.c
@@ -222,8 +222,9 @@ static Int READ_INNER ( UInt UseUHQ )
         ClearError();
         Obj evalResult;
         ExecStatus status = ReadEvalCommand(STATE(BottomLVars), &evalResult, 0);
-	if (STATE(UserHasQuit) || STATE(UserHasQUIT))
-	  break;
+        if (STATE(UserHasQuit) || STATE(UserHasQUIT))
+            break;
+
         /* handle return-value or return-void command                      */
         if ( status & (STATUS_RETURN_VAL | STATUS_RETURN_VOID) ) {
             Pr(
@@ -1680,10 +1681,9 @@ Obj FuncREAD_ALL_FILE (
     ifid = INT_INTOBJ(fid);
 
     while ( ! IS_INTOBJ(limit) ) {
-      limit = ErrorReturnObj(
-			     "<limit> must be a small integer (not a %s)",
-			     (Int)TNAM_OBJ(limit), 0L,
-			     "you can replace limit via 'return <limit>;'" );
+        limit = ErrorReturnObj("<limit> must be a small integer (not a %s)",
+                               (Int)TNAM_OBJ(limit), 0L,
+                               "you can replace limit via 'return <limit>;'");
     }
     ilim = INT_INTOBJ(limit);
 
@@ -1694,57 +1694,55 @@ Obj FuncREAD_ALL_FILE (
     lstr = 0;
 
 
-    if (syBuf[ifid].bufno >= 0)
-      {
-	UInt bufno = syBuf[ifid].bufno;
+    if (syBuf[ifid].bufno >= 0) {
+        UInt bufno = syBuf[ifid].bufno;
 
-	/* first drain the buffer */
-	lstr = syBuffers[bufno].buflen - syBuffers[bufno].bufstart;
-	if (ilim != -1)
-	  {
-	    if (lstr > ilim)
-	      lstr = ilim;
-	    ilim -= lstr;
-	  }
-	GROW_STRING(str, lstr);
-	memcpy(CHARS_STRING(str), syBuffers[bufno].buf + syBuffers[bufno].bufstart, lstr);
-	len = lstr;
-	SET_LEN_STRING(str, len);
-	syBuffers[bufno].bufstart += lstr;
-      }
+        /* first drain the buffer */
+        lstr = syBuffers[bufno].buflen - syBuffers[bufno].bufstart;
+        if (ilim != -1) {
+            if (lstr > ilim)
+                lstr = ilim;
+            ilim -= lstr;
+        }
+        GROW_STRING(str, lstr);
+        memcpy(CHARS_STRING(str),
+               syBuffers[bufno].buf + syBuffers[bufno].bufstart, lstr);
+        len = lstr;
+        SET_LEN_STRING(str, len);
+        syBuffers[bufno].bufstart += lstr;
+    }
 #ifdef SYS_IS_CYGWIN32
  getmore:
 #endif
     while (ilim == -1 || len < ilim ) {
       if ( len > 0 && !HasAvailableBytes(ifid))
-	break;
-      if (syBuf[ifid].isTTY)
-	{
-	  if (ilim == -1)
-	    {
-	      Pr("#W Warning -- reading to  end of input tty will never end\n",0,0);
-	      csize = 20000;
-	    }
-	  else
-	      csize = ((ilim- len) > 20000) ? 20000 : ilim - len;
-	    
-	  if (SyFgetsSemiBlock(buf, csize, ifid))
-	    lstr = strlen(buf);
-	  else  
-	    lstr = 0;
-	}
-      else
-	{
-	  do {
-	    csize = (ilim == -1 || (ilim- len) > 20000) ? 20000 : ilim - len;
-	    lstr = read(syBuf[ifid].fp, buf, csize);
-	  } while (lstr == -1 && errno == EAGAIN);
-	}
-      if (lstr <= 0)
-	{
-	  syBuf[ifid].ateof = 1;
-	  break;
-	}
+          break;
+      if (syBuf[ifid].isTTY) {
+          if (ilim == -1) {
+              Pr("#W Warning -- reading to  end of input tty will never "
+                 "end\n",
+                 0, 0);
+              csize = 20000;
+          }
+          else
+              csize = ((ilim - len) > 20000) ? 20000 : ilim - len;
+
+          if (SyFgetsSemiBlock(buf, csize, ifid))
+              lstr = strlen(buf);
+          else
+              lstr = 0;
+      }
+      else {
+          do {
+              csize =
+                  (ilim == -1 || (ilim - len) > 20000) ? 20000 : ilim - len;
+              lstr = read(syBuf[ifid].fp, buf, csize);
+          } while (lstr == -1 && errno == EAGAIN);
+      }
+      if (lstr <= 0) {
+          syBuf[ifid].ateof = 1;
+          break;
+      }
       GROW_STRING( str, len+lstr );
       memcpy(CHARS_STRING(str)+len, buf, lstr);
       len += lstr;
@@ -1755,28 +1753,22 @@ Obj FuncREAD_ALL_FILE (
     len = GET_LEN_STRING(str);
 #ifdef SYS_IS_CYGWIN32
     /* line end hackery */
-    {
-      UInt i = 0,j = 0;
-      while ( i < len )
-	{
-	  if (CHARS_STRING(str)[i] == '\r')
-	    {
-	      if (i < len -1 && CHARS_STRING(str)[i+1] == '\n')
-		{
-		  i++;
-		  continue;
-		}
-	      else
-		CHARS_STRING(str)[i] = '\n';
-	    }
-	  CHARS_STRING(str)[j++] = CHARS_STRING(str)[i++];
-	}
-      len = j;
-      SET_LEN_STRING(str, len);
-      if (ilim != -1 && len < ilim)
-	goto getmore;
-      
+    UInt i = 0, j = 0;
+    while (i < len) {
+        if (CHARS_STRING(str)[i] == '\r') {
+            if (i < len - 1 && CHARS_STRING(str)[i + 1] == '\n') {
+                i++;
+                continue;
+            }
+            else
+                CHARS_STRING(str)[i] = '\n';
+        }
+        CHARS_STRING(str)[j++] = CHARS_STRING(str)[i++];
     }
+    len = j;
+    SET_LEN_STRING(str, len);
+    if (ilim != -1 && len < ilim)
+        goto getmore;
 #endif
     ResizeBag( str, SIZEBAG_STRINGLEN(len) );
 
