2d0578a7e94381d375fe981cbc9258eb28450ed1
Max Horn <max@quendi.de>
2018-09-11 09:56:47 +0200

    Clarify MemoizePosIntFunction documentation, add a test
    Also clarify the documentation, and add a test case for when the
    invalidInputHandler returns without a value

--- lib/cache.gd
+++ lib/cache.gd
@@ -17,18 +17,19 @@
 ##  <Func Name="MemoizePosIntFunction" Arg='function [,options]'/>
 ##
 ##  <Description>
-##  <Ref Func="MemoizePosIntFunction"/> returns a function which behaves the same
-##  as <A>function</A>, except it caches the results; if the new function is
-##  called with the same input, then any call after the first will return the
-##  cached value, instead of recomputing it. The cache is flushed by calling
-##  <Ref Func="FlushCaches"/>.
+##  <Ref Func="MemoizePosIntFunction"/> returns a function which behaves the
+##  same as <A>function</A>, except it caches the results for any inputs that
+##  are positive integers. Thus if the new function is called multiple times
+##  with the same input, then any call after the first will return the cached
+##  value, instead of recomputing it. By default, the cache can be flushed by
+##  calling <Ref Func="FlushCaches"/>.
 ##  <P/>
-##  The returned function will only accept positive integers.
+##  The returned function will by default only accept positive integers.
 ##  <P/>
 ##  This function does not promise to never call <A>function</A> more than
 ##  once for any input -- values may be removed if the cache gets too large,
-##  or GAP chooses to flush all caches, or if multiple threads try to calculate
-##  the same value simultaneously.
+##  or if <Ref Func="FlushCaches"/> is called, or if multiple threads try to
+##  calculate the same value simultaneously.
 ##  <P/>
 ##  The optional second argument is a record which provides a number
 ##  of configuration options. The following options are supported.
@@ -36,17 +37,21 @@
 ##  <Mark><C>defaults</C> (default an empty list)</Mark>
 ##  <Item>
 ##    Used to initalise the cache, both initially and after each flush.
+##    If <C>defaults[i]</C> is bound, then this is used as default vale
+##    for the input <C>i</C>.
 ##  </Item>
 ##  <Mark><C>flush</C> (default <K>true</K>)</Mark>
 ##  <Item>
 ##    If this is <K>true</K>, the cache is emptied whenever
-##    <Ref Func="FlushCaches"/> is called.
+##    <Ref Func="FlushCaches"/> is called; if false, then the cache
+##    cannot be flushed.
 ##  </Item>
 ##  <Mark><C>errorHandler</C> (defaults to <Ref Func="Error"/>)</Mark>
 ##  <Item>
 ##    A function to be called when an input which is not a positive integer
-##    is passed to the cache. If this function returns a value, that
-##    value is returned by the cache.
+##    is passed to the cache. The function can either raise an error, or else
+##    return a value which is then returned by the cache. Note that such a
+##    value does not get cached itself.
 ##  </Item>
 ##  </List>
 ##  <P/>
