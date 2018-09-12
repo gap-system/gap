54c34b7938b4eba0ba94c5381e6b8c3d689956fd
Max Horn <max@quendi.de>
2018-09-12 14:56:02 +0200

    kernel: unify 'and' for filters in interpreter and executor
    Also start using IS_FILTER. Note that previously, this input was silently accepted:
    
      gap> Center and IsAssociative;
      <Filter "<<and-filter>>">
    
    This was effectively treated equivalently to `HasCenter and IsAssociative;`.
    We now could reject such code. But unfortunately a few packages accidentally
    relied on this bug. We thus only print a warning for now, and will turn this
    into a proper error only once all affect packages released a fixed version.

--- src/exprs.c
+++ src/exprs.c
@@ -183,15 +183,27 @@ Obj             EvalAnd (
     }
 
     /* handle the 'and' of two filters                                    */
-    else if ( TNUM_OBJ(opL) == T_FUNCTION ) {
+    else if (IS_PSEUDO_FILTER(opL)) {
+        if (!IS_FILTER(opL)) {
+            // support this for backwards compatibility; see discussion on
+            // https://github.com/gap-system/gap/pull/2732
+            Warning("operation '%g' used in AND-filter is not a filter",
+                    (Int)NAME_FUNC(opL), 0);
+        }
         tmp = READ_EXPR(expr, 1);
         opR = EVAL_EXPR( tmp );
-        if ( TNUM_OBJ(opR) == T_FUNCTION ) {
+        if (IS_PSEUDO_FILTER(opR)) {
+            if (!IS_FILTER(opR)) {
+                // support this for backwards compatibility; see discussion on
+                // https://github.com/gap-system/gap/pull/2732
+                Warning("operation '%g' used in AND-filter is not a filter",
+                        (Int)NAME_FUNC(opR), 0);
+            }
             return NewAndFilter( opL, opR );
         }
         else {
             ErrorQuit(
-                "<expr> must be 'true' or 'false' (not a %s)",
+                "<expr> must be a filter (not a %s)",
                 (Int)TNAM_OBJ(opL), 0L );
         }
     }
@@ -199,7 +211,7 @@ Obj             EvalAnd (
     /* signal an error                                                     */
     else {
         ErrorQuit(
-            "<expr> must be 'true' or 'false' (not a %s)",
+            "<expr> must be 'true' or 'false' or a filter (not a %s)",
             (Int)TNAM_OBJ(opL), 0L );
     }
     
