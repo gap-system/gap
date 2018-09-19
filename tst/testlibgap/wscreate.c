a2b733ac5842090312717a40414da93525e68ed6
Dima Pasechnik <dimpase@gmail.com>
2018-09-19 13:11:57 +0100

    testing for libgap saving/loading workspaces
    Currently loading does not work due to GAP issue #2832

--- tst/testlibgap/wscreate.c
+++ tst/testlibgap/wscreate.c
@@ -4,6 +4,7 @@
 #include "common.h"
 int main(int argc, char ** argv)
 {
+    char ws[80] = "SaveWorkspace(\"/tmp/libgap.ws\");\n";
     printf("# Initializing GAP...\n");
     GAP_Initialize(argc, argv, environ, 0L, 0L);
     CollectBags(0, 1);    // full GC
@@ -14,7 +15,7 @@ int main(int argc, char ** argv)
     test_eval("lis:=[a^2, a^2, b*a];");
     test_eval("h:=g/lis;");
     test_eval("c:=h.1;");
-    test_eval("Set([1..1000000], i->Order(c));");
+    test_eval(ws);
     printf("# done\n");
     return 0;
 }
