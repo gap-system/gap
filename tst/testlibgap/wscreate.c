8ba82ab6bf7dfa532f45fd7e4a4936229a4888c0
Dima Pasechnik <dimpase@gmail.com>
2018-09-20 12:46:16 +0100

    created expected output files; added cleanup to rules

--- tst/testlibgap/wscreate.c
+++ tst/testlibgap/wscreate.c
@@ -4,18 +4,14 @@
 #include "common.h"
 int main(int argc, char ** argv)
 {
-    char ws[80] = "SaveWorkspace(\"/tmp/libgap.ws\");\n";
-    printf("# Initializing GAP...\n");
     GAP_Initialize(argc, argv, environ, 0L, 0L);
     CollectBags(0, 1);    // full GC
-    test_eval("1+2+3;");
     test_eval("g:=FreeGroup(2);");
     test_eval("a:=g.1;");
     test_eval("b:=g.2;");
     test_eval("lis:=[a^2, a^2, b*a];");
     test_eval("h:=g/lis;");
-    test_eval("c:=h.1;");
-    test_eval(ws);
+    test_eval("SaveWorkspace(\"/tmp/libgap.ws\");\n");
     printf("# done\n");
     return 0;
 }
