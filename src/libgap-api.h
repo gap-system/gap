c8fb501c4cd787c4233e373744d58a3ad73c28a9
Max Horn <max@quendi.de>
2018-10-23 13:12:15 +0200

    Update src/libgap-api.h
    Co-Authored-By: sebasguts <gutsche@mathematik.uni-siegen.de>

--- src/libgap-api.h
+++ src/libgap-api.h
@@ -17,7 +17,7 @@ void GAP_Initialize(int          argc,
 
 Obj GAP_ValueGlobalVariable(const char * name);
 Obj GAP_EvalString(const char * cmd);
-Obj GAP_MakeString(char * string);
+Obj GAP_MakeString(const char * string);
 char * GAP_CSTR_STRING(Obj string);
 Obj GAP_NewPlist(Int capacity);
 
