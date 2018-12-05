gap-system/gap

commit 51d898ca7ded76ed76dead486648dd121b40fdf9

Take advantage of implications to simplify some code

lib/grp.gi
@@ -4316,12 +4316,12 @@ function(fam,filt,gens,id,isgroup)
       fi;
       if id<>fail then # cannot do identity in magma
         if ForAny(gens,x->x<>id) then
-          filt:=filt and HasIsTrivial and IsNonTrivial;
+          filt:=filt and IsNonTrivial;
           if isgroup and Length(gens)<=1 then # cyclic not for magmas
             filt:=filt and IsCyclic;
           fi;
         else
-          filt:=filt and IsTrivial and HasIsNonTrivial;
+          filt:=filt and IsTrivial;
         fi;
       fi;
     elif isgroup and Length(gens)<=1 then # cyclic not for magmas
@@ -4379,8 +4379,7 @@ local G,fam,typ;
   fam:= CollectionsFamily( FamilyObj( id ) );
 
   typ:=IsGroup and IsAttributeStoringRep
-        and HasGeneratorsOfMagmaWithInverses and HasOne and IsTrivial and
-        HasIsEmpty and HasIsNonTrivial;
+        and HasGeneratorsOfMagmaWithInverses and HasOne and IsTrivial;
   typ:=NewType(fam,typ);
 
   G:= rec();

lib/coll.gi
@@ -3079,7 +3079,7 @@ local filt;
     fi;
     return;
   fi;
-  if sz=0 then filt:=IsEmpty and IsNonTrivial; # IsNonTrivial hold
+  if sz=0 then filt:=IsEmpty;
   elif sz=1 then filt:=IsTrivial;
   elif sz=infinity then filt:=IsNonTrivial and HasIsFinite;
   else filt:=IsNonTrivial and IsFinite;

lib/grpmat.gi
@@ -868,9 +868,7 @@ local G,fam,typ,f;
 
   if not IsFinite(gens) then TryNextMethod(); fi;
   typ:=MakeGroupyType(FamilyObj([id]), IsGroup and IsAttributeStoringRep 
-            and HasGeneratorsOfMagmaWithInverses and IsFinitelyGeneratedGroup
-            and HasIsEmpty and IsFinite and HasOne
-            and IsTrivial and HasIsNonTrivial,
+            and HasGeneratorsOfMagmaWithInverses and HasOne and IsTrivial,
             gens,id,true);
 
   f:=DefaultScalarDomainOfMatrixList([id]);

lib/grppc.gi
@@ -412,10 +412,7 @@ local G,fam,typ,pcgs;
 
   # pc groups are always finite and gens is finite.
   typ:=IsGroup and IsAttributeStoringRep 
-        and HasGeneratorsOfMagmaWithInverses and HasOne
-        and IsFinite and IsFinitelyGeneratedGroup
-        and IsTrivial and HasIsNonTrivial
-        and HasIsEmpty and IsSolvableGroup
+        and HasGeneratorsOfMagmaWithInverses and HasOne and IsTrivial
         and HasFamilyPcgs and HasHomePcgs and HasGeneralizedPcgs;
   typ:=NewType(fam,typ);
 

lib/modulrow.gi
@@ -35,9 +35,9 @@ local M,typ;   # the free module record, result
     fi;
 
     if n=0 then
-      typ:=typ and IsTrivial and HasIsNonTrivial and IsFinite;
+      typ:=typ and IsTrivial;
     else
-      typ:=typ and HasIsTrivial and IsNonTrivial;
+      typ:=typ and IsNonTrivial;
     fi;
 
     if n<>infinity and HasIsFinite(R) and IsFinite(R) then

lib/vspcrow.gi
@@ -64,9 +64,9 @@ InstallMethod( LeftModuleByGenerators,
     fi;
 
     if Length(mat)>0 and ForAny(mat,x->not IsZero(x)) then
-      typ:=typ and HasIsTrivial and IsNonTrivial;
+      typ:=typ and IsNonTrivial;
     else
-      typ:=typ and IsTrivial and HasIsNonTrivial;
+      typ:=typ and IsTrivial;
     fi;
 
     if HasIsFinite(F) then
@@ -98,9 +98,7 @@ InstallMethod( LeftModuleByGenerators,
     fi;
 #T explicit 2nd argument above!
 
-    typ:=IsAttributeStoringRep and HasIsEmpty and IsFiniteDimensional
-         and IsGaussianRowSpace and IsTrivial and HasIsNonTrivial and
-         IsFinite;
+    typ:=IsAttributeStoringRep and IsGaussianRowSpace and IsTrivial;
 
     V:= Objectify( NewType( CollectionsFamily( FamilyObj( F ) ),typ),
                    rec() );
@@ -132,9 +130,9 @@ InstallMethod( LeftModuleByGenerators,
     fi;
 
     if Length(mat)>0 and ForAny(mat,x->not IsZero(x)) then
-      typ:=typ and HasIsTrivial and IsNonTrivial;
+      typ:=typ and IsNonTrivial;
     else
-      typ:=typ and IsTrivial and HasIsNonTrivial;
+      typ:=typ and IsTrivial;
     fi;
 
     if HasIsFinite(F) then

commit 8445200843e6b81a841b75137decbb30152dcade

call `GroupByGenerators`, `GroupWithGenerators` with domain
change the default methods for `GroupByGenerators`
and `GroupWithGenerators` such that the problem gets fixed
that had been introduced with pull request #2522
and discussed in issue #2703

lib/grp.gi
@@ -4398,55 +4398,48 @@ end);
 InstallMethod( GroupWithGenerators,
     "generic method for collection",
     [ IsCollection ],
-function( gens )
-local G,typ;
+    function( gens )
+    local typ;
 
-  typ:=MakeGroupyType(FamilyObj(gens),
+    gens:= AsList( gens );
+    typ:= MakeGroupyType( FamilyObj( gens ),
           IsGroup and IsAttributeStoringRep 
           and HasIsEmpty and HasGeneratorsOfMagmaWithInverses,
           gens,false,true);
 
-  G:=rec();
-  ObjectifyWithAttributes(G,typ,GeneratorsOfMagmaWithInverses,AsList(gens));
-
-  return G;
+    return ObjectifyWithAttributes( rec(), typ,
+               GeneratorsOfMagmaWithInverses, gens );
 end );
 
 InstallMethod( GroupWithGenerators,
     "generic method for collection and identity element",
-    IsCollsElms, [ IsCollection, IsMultiplicativeElementWithInverse ],
-function( gens, id )
-local G,typ;
+    IsCollsElms,
+    [ IsCollection, IsMultiplicativeElementWithInverse ],
+    function( gens, id )
+    local typ;
 
-  typ:=MakeGroupyType(FamilyObj(gens),
+    gens:= AsList( gens );
+    typ:= MakeGroupyType( FamilyObj( gens ),
           IsGroup and IsAttributeStoringRep 
             and HasIsEmpty and HasGeneratorsOfMagmaWithInverses and HasOne,
             gens,id,true);
 
-  G:=rec();
-  ObjectifyWithAttributes(G,typ,GeneratorsOfMagmaWithInverses,AsList(gens),
-                          One,id);
-
-  return G;
+    return ObjectifyWithAttributes( rec(), typ,
+               GeneratorsOfMagmaWithInverses, gens, One, id );
 end );
 
-InstallMethod( GroupWithGenerators,"method for empty list and element",
-  [ IsList and IsEmpty, IsMultiplicativeElementWithInverse ],
-  function( empty, id )
-local G,fam,typ;
+InstallMethod( GroupWithGenerators,
+    "method for empty list and element",
+    [ IsList and IsEmpty, IsMultiplicativeElementWithInverse ],
+    function( empty, id )
+    local typ;
 
-  fam:= CollectionsFamily( FamilyObj( id ) );
+    typ:= NewType( CollectionsFamily( FamilyObj( id ) ),
+              IsGroup and IsAttributeStoringRep and
+              HasGeneratorsOfMagmaWithInverses and HasOne and IsTrivial );
 
-  typ:=IsGroup and IsAttributeStoringRep
-        and HasGeneratorsOfMagmaWithInverses and HasOne and IsTrivial;
-  typ:=NewType(fam,typ);
-
-  G:= rec();
-  ObjectifyWithAttributes( G, typ,
-                            GeneratorsOfMagmaWithInverses, empty,
-                            One, id );
-
-  return G;
+    return ObjectifyWithAttributes( rec(), typ,
+               GeneratorsOfMagmaWithInverses, empty, One, id );
 end );
 
 
@@ -4456,14 +4449,45 @@ end );
 #M  GroupByGenerators( <gens>, <id> )
 ##
 InstallMethod( GroupByGenerators,
-    "delegate to `GroupWithGenerators'",
+    "replace domain by suitable list, and call `GroupWithGenerators'",
     [ IsCollection ],
+    function( gens )
+    if HasGeneratorsOfMagmaWithInverses( gens ) then
+      gens:= GeneratorsOfMagmaWithInverses( gens );
+    elif HasGeneratorsOfMagmaWithOne( gens ) then
+      gens:= GeneratorsOfMagmaWithOne( gens );
+    elif HasGeneratorsOfMagma( gens ) then
+      gens:= GeneratorsOfMagma( gens );
+    fi;
+
+    return GroupWithGenerators( gens );
+    end );
+
+InstallMethod( GroupByGenerators,
+    "replace domain by suitable list, and call `GroupWithGenerators'",
+    IsCollsElms,
+    [ IsCollection, IsMultiplicativeElementWithInverse ],
+    function( gens, id )
+    if HasGeneratorsOfMagmaWithInverses( gens ) then
+      gens:= GeneratorsOfMagmaWithInverses( gens );
+    elif HasGeneratorsOfMagmaWithOne( gens ) then
+      gens:= GeneratorsOfMagmaWithOne( gens );
+    elif HasGeneratorsOfMagma( gens ) then
+      gens:= GeneratorsOfMagma( gens );
+    fi;
+
+    return GroupWithGenerators( gens, id );
+    end );
+
+InstallMethod( GroupByGenerators,
+    "delegate to `GroupWithGenerators'",
+    [ IsCollection and IsList ],
     GroupWithGenerators );
 
 InstallMethod( GroupByGenerators,
     "delegate to `GroupWithGenerators'",
     IsCollsElms,
-    [ IsCollection, IsMultiplicativeElementWithInverse ],
+    [ IsCollection and IsList, IsMultiplicativeElementWithInverse ],
     GroupWithGenerators );
 
 InstallMethod( GroupByGenerators,