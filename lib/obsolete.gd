82bbc014096036a0ad040a3e264df675acd12725
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2018-12-13 12:56:01 +0000

    Do not include ManSection for IsSemilatticeAsSemigroup
    IsSemilatticeAsSemigroup is not available any more even when
    obsoletes are loaded. It is now available as IsSemilattice
    in the Semigroups package.

--- lib/obsolete.gd
+++ lib/obsolete.gd
@@ -560,7 +560,6 @@ DeclareOperation( "LaTeXObj", [ IS_OBJECT ] );
 ##
 #F  IsSemilatticeAsSemigroup( <S> ) is the semigroup <S> a semilattice
 ##
-##  <#GAPDoc Label="IsSemilatticeAsSemigroup">
 ##  <ManSection>
 ##  <Prop Name="IsSemilatticeAsSemigroup" Arg='S'/>
 ##
@@ -577,7 +576,6 @@ DeclareOperation( "LaTeXObj", [ IS_OBJECT ] );
 ##    beta-releases.  #  It should <E>not</E> be used in new code.
 ##  </Description>
 ##  </ManSection>
-##  <#/GAPDoc>
 ##
 ##  Not used in any redistributed packages (11/2017)
 #DeclareSynonymAttr( "IsSemilatticeAsSemigroup", IsSemilattice );
