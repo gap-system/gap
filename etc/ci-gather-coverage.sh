4616886ac8adf04dee7a88f05c9774093aa64102
Max Horn <max@quendi.de>
2018-12-21 11:59:32 +0100

    Submit GAP code coverage to coveralls.io
    Results can be seen on https://coveralls.io/github/gap-system/gap

--- etc/ci-gather-coverage.sh
+++ etc/ci-gather-coverage.sh
@@ -49,8 +49,23 @@ od;
 if Length(covs) > 0 then
     Print("Merging ", Length(covs), " coverage files...\n");
     r := MergeLineByLineProfiles(covs);;
-    Print("Outputting JSON\n");
+
+    # now remove "weird" entries, and entries for pkg files
+    prefix := Concatenation(GAPInfo.SystemEnvironment.PWD, "/");
+    r.line_info := Filtered(r.line_info, file ->
+        StartsWith(file[1], prefix) and
+        fail = PositionSublist(file[1], "/pkg/") and
+        IsReadableFile(file[1]) );
+
+    Print("Outputting JSON for Codecov...\n");
     OutputJsonCoverage(r, "gap-coverage.json");;
+
+    if IsBound(GAPInfo.SystemEnvironment.TRAVIS_JOB_ID) then
+        Print("Outputting JSON for Coveralls...\n");
+        OutputCoverallsJsonCoverage(r, "gap-coveralls.json",
+                GAPInfo.SystemEnvironment.TRAVIS_JOB_ID,
+                prefix);
+    fi;
 else
     # Don't error, because we might want to gather
     # gcov coverage, so just inform that we didn't find
@@ -61,3 +76,9 @@ GAPInput
 
 # generate kernel coverage reports by running gcov
 make coverage
+
+# upload to coveralls.io
+if [[ -f gap-coveralls.json ]]
+then
+  curl -F json_file=@gap-coveralls.json "https://coveralls.io/api/v1/jobs"
+fi
