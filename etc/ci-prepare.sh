7f939803861186965e8ddf3bf2faea2934164ea9
Max Horn <max@quendi.de>
2019-01-22 17:11:35 +0100

    Switch Julia builds to 1.1.0

--- etc/ci-prepare.sh
+++ etc/ci-prepare.sh
@@ -34,9 +34,8 @@ fi
 
 if [[ $JULIA = yes ]]
 then
-  # TODO: once Julia 1.1 is released, switch to stable Julia versions here?
-  # or maybe use Travis' `language: julia` feature?
-  wget https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-rc2-linux-x86_64.tar.gz
+  pushd extern
+  wget https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz
   tar xvf julia-*.tar.gz
   rm julia-*.tar.gz
   pushd julia-*
