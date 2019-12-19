c10d5a4902724c9cd57b2e46333b7b54e5b58949
Chris Jefferson <caj21@st-andrews.ac.uk>
2019-12-19 18:59:55 +0000

    Add comment explaining how tracing works

--- src/tracing.h
+++ src/tracing.h
@@ -10,15 +10,45 @@
 **  This file defines the functions for tracing operations.
 */
 
+#ifndef GAP_TRACING_H
+#define GAP_TRACING_H
+
 #include "objects.h"
 
-
+// The following functions should not be called directly. They are provided
+// so the macros below function correctly
 void ReportWrappedOperation1(const char *, Obj op);
 void ReportWrappedOperation2(const char *, Obj op1, Obj op2);
 void InstallOpWrapper(voidfunc, voidfunc);
 
 
-// These are macros which make it simple to wrap
+// Many built-in operators in GAP are implemented as a 1 or 2 dimensional
+// array of function pointers, where we set one function for each possible
+// TNUM (or pair of TNUMs) which can be given to the operator.
+
+// These macros allow us to wrap these array of function pointers, so every
+// time one of the functions in the array is called, an extra function is also
+// called, which can be used for tracking which operators are called with
+// which arguments. This allows us to track when these operators are used
+// without paying a cost when the operators are not being tracked.
+
+// These should be installed using the macros below.
+
+// Firstly the array of function pointers should be passed to
+// DEFINE_OP_WRAPPER1/2 at the global scope. Given an array 'Array', these
+// macros:
+//
+// 1) Defines an array 'WrapArray', to store the contents of Array
+//    while it is wrapped.
+// 2) A function 'WrapArrayFunc'. We fill 'Array' with pointers to this
+//    function when wrapping is active, which means ReportWrappedFunction
+//    gets called whenever we try to call one of the members of 'Array'.
+// 3) ArrayHookActive and ArrayHookDeactive, to turn the wrapping on and off.
+
+// Note that calling DEFINE_OP_WRAPPER1/2 (for 1/2 dimensional arrays
+// respectively) only define functions. We must also call INSTALL_OP_WRAPPER
+// so tracing.c is informed that 'Array' can be wrapped.
+
 
 #define DEFINE_OP_WRAPPER1(Array)                                            \
     static ObjFunc_0ARGS Wrap##Array[LAST_REAL_TNUM + 1];                    \
@@ -34,7 +64,6 @@ void InstallOpWrapper(voidfunc, voidfunc);
             Array[i] = Wrap##Array##Func;                                    \
         }                                                                    \
     }                                                                        \
-                                                                             \
     void Array##HookDeactivate(void)                                         \
     {                                                                        \
         for (int i = 0; i < LAST_REAL_TNUM; ++i) {                           \
@@ -60,7 +89,6 @@ void InstallOpWrapper(voidfunc, voidfunc);
             }                                                                \
         }                                                                    \
     }                                                                        \
-                                                                             \
     void Array##HookDeactivate(void)                                         \
     {                                                                        \
         for (int i = 0; i < LAST_REAL_TNUM; ++i) {                           \
@@ -80,3 +108,5 @@ void InstallOpWrapper(voidfunc, voidfunc);
 *F  InitInfoTracing() . . . . . . . . . . . . . . . . table of init functions
 */
 StructInitInfo * InitInfoTracing(void);
+
+#endif
