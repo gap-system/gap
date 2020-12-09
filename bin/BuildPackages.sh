e157e8967990fd40cf081b21b7cc074ed78b532e
ThomasBreuer <sam@math.rwth-aachen.de>
2020-12-09 12:55:09 +0100

    Call GAP with `-r` option ...
    ... when asking for printed output,
    since `~/.gap/gaprc` may contain `Print` statements.

--- bin/BuildPackages.sh
+++ bin/BuildPackages.sh
@@ -179,7 +179,7 @@ run_configure_and_make() {
   then
     if grep Autoconf ./configure > /dev/null
     then
-      local PKG_NAME=$($GAP -q -T -A <<GAPInput
+      local PKG_NAME=$($GAP -q -T -A -r <<GAPInput
 Read("PackageInfo.g");
 Print(GAPInfo.PackageInfoCurrent.PackageName);
 GAPInput
