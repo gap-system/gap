aec4b3994def79652b5ed90a49dd3e462fec353f
Sergio Siccha <sergio.siccha@posteo.net>
2021-03-02 18:04:59 +0100

    make release scripts more robust

--- dev/releases/make_github_release.py
+++ dev/releases/make_github_release.py
@@ -3,51 +3,56 @@
 # This script is intended to implement step 6 of
 # <https://hackmd.io/AWds-AnZT72XXsbA0oVC6A>, i.e.
 # it makes a github release and uploads all tar balls as assets.
-# The target repository is hardcoded in utils.py:
-#   CURRENT_REPO_NAME
-#
-# As in make_archives.py, the version of the gap release is taken from the
-# Makefile variable GAP_BUILD_VERSION.
+# The name of the target repository CURRENT_REPO_NAME is defined in utils.py.
 
 # If we do import * from utils, then initialize_github can't overwrite the
 # global GITHUB_INSTANCE and CURRENT_REPO variables.
 import utils
 import github
 import os
+import sys
 import re
 
-# Identify GAP release version
-try:
-    GAPVERSION = utils.get_makefile_var("GAP_BUILD_VERSION")
-except:
-    utils.error("Could not get GAP version")
+if len(sys.argv) != 3:
+    utils.error("usage: "+sys.argv[0]+" <tag_name> <path_to_release>")
 
-utils.notice(f"Detected GAP version {GAPVERSION}")
-if re.fullmatch( r"[1-9]+\.[0-9]+\.[0-9]+", GAPVERSION) == None:
-    utils.error("This does not look a release version")
+utils.verify_git_clean()
 
+TAG_NAME = sys.argv[1]
+PATH_TO_RELEASE = sys.argv[2]
 
+if re.fullmatch( r"v[1-9]+\.[0-9]+\.[0-9]+", TAG_NAME) == None:
+    utils.error("This does not look like a release version")
+
+# make sure that TAG_NAME
+# - exists
+# - is an annotated tag
+# - points to current HEAD
+utils.check_git_tag_for_release(TAG_NAME)
+
+# Initialize GITHUB_INSTANCE and CURRENT_REPO
 utils.initialize_github()
 
 # Error if this release has been already created on GitHub
-if utils.check_whether_github_release_exists("v"+GAPVERSION):
-    utils.error(f"Release v{GAPVERSION} already exists!")
+if any(r.tag_name == TAG_NAME for r in utils.CURRENT_REPO.get_releases()):
+    utils.error(f"Github release with tag '{TAG_NAME}' already exists!")
 
 # Create release
-CURRENT_BRANCH = utils.get_makefile_var("PKG_BRANCH")
-RELEASE_NOTE = f"For an overview of changes in GAP {GAPVERSION} see the " \
-    + "[CHANGES.md](https://github.com/gap-system/gap/blob/master/CHANGES.md) file."
-utils.notice(f"Creating release v{GAPVERSION}")
-RELEASE = utils.CURRENT_REPO.create_git_release("v"+GAPVERSION, "v"+GAPVERSION,
+RELEASE_NOTE = f"For an overview of changes in GAP {TAG_NAME[1:]} see the " \
+    + f"[CHANGES.md](https://github.com/gap-system/gap/blob/{TAG_NAME}/CHANGES.md) file."
+utils.notice(f"Creating release {TAG_NAME}")
+RELEASE = utils.CURRENT_REPO.create_git_release(TAG_NAME, TAG_NAME,
                                                 RELEASE_NOTE,
-                                                target_commitish=CURRENT_BRANCH,
                                                 prerelease=True)
 
-tmpdir = os.getcwd() + "/tmp"
-with utils.working_directory(tmpdir):
+with utils.working_directory(PATH_TO_RELEASE):
     manifest_filename = "MANIFEST"
     with open(manifest_filename, 'r') as manifest_file:
         manifest = manifest_file.read().splitlines()
+    # Now check that TAG_NAME and the created archives belong together
+    main_archive_name = "gap-" + TAG_NAME[1:] + ".tar.gz"
+    if not main_archive_name in manifest:
+        error(f"Expected a file {main_archive_name} but it does not exist!")
     # Upload all assets to release
     try:
         for filename in manifest:
