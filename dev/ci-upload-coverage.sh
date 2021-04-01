43876a7ebabbaf726b9bfe8ba6530625af813613
Max Horn <max@quendi.de>
2021-04-02 01:17:32 +0200

    CI: fix coverage reporting
    A bunch of other io.c files exist (e.g. in the IO package and in GMP) and any
    coverage data for them can confuse Codecov, which may merge it with that for
    GAP's `src/io.c`. Tell codeco.vsh very sternly to ignore coverage data coming
    from the `extern` and `pkg` directories.

--- dev/ci-upload-coverage.sh
+++ dev/ci-upload-coverage.sh
@@ -13,4 +13,4 @@ fi
 # upload to Codecov
 curl -s https://codecov.io/bash > codecov.sh
 chmod +x codecov.sh
-./codecov.sh -f '!./pkg/*' -f '!./extern/*' -f '!./build/*'
+./codecov.sh -f '!./pkg/**' -f '!./extern/**' -g './pkg/**' -g './extern/**'
