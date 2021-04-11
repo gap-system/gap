d09fe82514cd2815769cb14924b82ec21dbe256b
Wilf Wilson <wilf@wilf-wilson.net>
2021-04-11 11:03:38 +0100

    Create Cygwin installers in GitHub Actions

---
+++ dev/releases/download_release_archive.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# This script is supposed to download the release asset with name <asset> to
+# the directory <download_dir> from the GitHub release at utils.CURRENT_REPO
+# with the tag <tag_name>.
+
+# It checks that <download_dir> exists, that <tag_name> is a release, and that
+# <asset> is the name of a file attached to the release.
+
+import os
+import sys
+import utils
+
+if len(sys.argv) != 4:
+    utils.error("usage: "+sys.argv[0]+" <tag_name> <asset> <download_dir>")
+
+TAG_NAME = sys.argv[1]
+ASSET = sys.argv[2]
+DOWNLOAD_DIR = sys.argv[3]
+
+if not os.path.isdir(DOWNLOAD_DIR):
+    utils.error(f"{DOWNLOAD_DIR} seems not to be a directory")
+utils.verify_is_possible_gap_release_tag(TAG_NAME)
+utils.initialize_github()
+
+try:
+    RELEASE = utils.CURRENT_REPO.get_release(TAG_NAME)
+except:
+    utils.error(f"{utils.CURRENT_REPO_NAME} contains no release {TAG_NAME}")
+
+assets = [ x for x in RELEASE.get_assets() if ASSET == x.name ]
+if len(assets) != 1:
+    utils.error(f"No unique file {FILE} exists on the release {TAG_NAME}")
+
+utils.download_with_sha256(assets[0].browser_download_url, os.path.realpath(DOWNLOAD_DIR) + "/" + ASSET)
---
+++ dev/releases/upload_files_to_github_release.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# This script is supposed to upload the files at <path_to_file1> [...] to the
+# GitHub release at utils.CURRENT_REPO with the tag <tag_name>.
+
+# It checks that each <path_to_file1> (etc) is a file, that <tag_name> is a
+# release, and that there are not already any files with the same name attached
+# to the release.
+
+import os
+import sys
+import utils
+
+if len(sys.argv) < 3:
+    utils.error("usage: "+sys.argv[0]+" <tag_name> <path_to_file1> [...]")
+
+TAG_NAME = sys.argv[1]
+ASSETS = sys.argv[2:]
+
+for ASSET in ASSETS:
+    if not os.path.isfile(ASSET):
+        utils.error(f"{ASSET} not found")
+
+utils.verify_is_possible_gap_release_tag(TAG_NAME)
+utils.initialize_github()
+
+try:
+    RELEASE = utils.CURRENT_REPO.get_release(TAG_NAME)
+except:
+    utils.error(f"{utils.CURRENT_REPO_NAME} contains no release {TAG_NAME}")
+
+for ASSET in ASSETS:
+    FILE = os.path.basename(ASSET)
+    if any(FILE == x.name for x in RELEASE.get_assets()):
+        utils.error(f"A file {FILE} already exists on the release {TAG_NAME}")
+
+for ASSET in ASSETS:
+    utils.upload_asset_with_checksum(RELEASE, ASSET)
