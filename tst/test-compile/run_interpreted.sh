697fb3df7cbb367f1b0c122d259375218e44aa3c
Max Horn <max@quendi.de>
2021-06-26 02:13:26 +0200

    Improve testspecial and test-compile scripts
    - change `run_gap.sh` to directly create its output file, instead of
      printing to stdout and then piping *that* into the output file; this
      makes it less likely that `regenerate_tests.sh` leaves corrupt `.out`
      files when interrupted
    - make use of `gap --print-gaproot` for more robust code

--- tst/test-compile/run_interpreted.sh
+++ tst/test-compile/run_interpreted.sh
@@ -6,6 +6,7 @@ set -e
 # It reads gapfile.g, then runs the function 'runtest'
 gap="$1"
 gfile="$2"
+outfile="$3"
 
 # It provides the following features:
 # 1) Stop GAP from attaching to the terminal (which it will
@@ -13,7 +14,8 @@ gfile="$2"
 # 2) Combine stderr and stdout
 # 3) Rewrite the root of gap with the string GAPROOT,
 #    so the output is usable on other machines
-GAPROOT=$(cd ../..; pwd)
+GAPROOT=$("$gap" --print-gaproot)
 echo "Read(\"$gfile\"); runtest();" |
     "$gap" -r -A -q -b -x 200 2>&1 |
-    sed "s:${GAPROOT//:/\\:}:GAPROOT:g"
+    sed "s:${GAPROOT//:/\\:}:GAPROOT/:g" > $outfile.tmp
+mv $outfile.tmp $outfile
--- tst/testspecial/run_gap.sh
+++ tst/testspecial/run_gap.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-# This script should be run as ./run_gap.sh gap gapfile.g
+# This script should be run as ./run_gap.sh gap gapfile.g [gapfile.g.out]
 gap="$1"
 gfile="$2"
+outfile="${3:-$gfile.out}"
 
 # It provides the following features:
 # 1) Stop GAP from attaching to the terminal (which it will
@@ -13,10 +14,10 @@ gfile="$2"
 # 3) Rewrite the root of gap with the string GAPROOT,
 #    so the output is usable on other machines
 # 4) Set lower and upper memory limits, for consistency
-GAPROOT=$(cd ../..; pwd)
+GAPROOT=$("$gap" --print-gaproot)
 ( echo "LogTo(\"${outfile}.tmp\");" ; cat "$gfile" ; echo "QUIT;" ) |
     "$gap" -r -A -b -m 256m -o 512m -x 800 \
            -c 'SetUserPreference("UseColorsInTerminal",false);' \
            2>/dev/null >/dev/null
-sed -E -e "s:${GAPROOT//:/\\:}:GAPROOT:g" -e "s;(GAPROOT(/[^/]+)+):[0-9]+;\1:LINE;g" < "${outfile}.tmp"
+sed -E -e "s:${GAPROOT//:/\\:}:GAPROOT/:g" -e "s;(GAPROOT(/[^/]+)+):[0-9]+;\1:LINE;g" < "${outfile}.tmp" > "${outfile}"
 rm "${outfile}.tmp"
