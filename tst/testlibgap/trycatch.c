4f84c49c03cdd1206ce195b9a6e70a1152fd3a35
Max Horn <max@quendi.de>
2021-07-26 12:16:07 +0200

    Change tst/testlibgap/trycatch to track situation more comprehensively

--- tst/testlibgap/trycatch.c
+++ tst/testlibgap/trycatch.c
@@ -11,17 +11,14 @@ static void handle_trycatch(TryCatchMode mode)
     switch (mode) {
     case TryEnter:
         level++;
-        if (level == 1)
-            printf("Entering GAP_TRY section\n");
+        printf("%d: Entering GAP_TRY section\n", level);
         break;
     case TryLeave:
-        if (level == 1)
-            printf("Leaving GAP_TRY section\n");
+        printf("%d: Leaving GAP_TRY section\n", level);
         level--;
         break;
     case TryCatch:
-        if (level == 1)
-            printf("Caught error in GAP_TRY section\n");
+        printf("%d: Caught error in GAP_TRY section\n", level);
         level--;
         break;
     }
@@ -31,11 +28,12 @@ int main(int argc, char ** argv)
 {
     printf("# Initializing GAP...\n");
     GAP_Initialize(argc, argv, 0, 0, 1);
-    RegisterTryCatchHandler(handle_trycatch);
     test_eval("OnBreak := false;;");
     // Necessary to redirect error printing to stdout.
     test_eval("MakeReadWriteGVar(\"ERROR_OUTPUT\");");
     test_eval("ERROR_OUTPUT := MakeImmutable(\"*stdout*\");;");
+
+    RegisterTryCatchHandler(handle_trycatch);
     test_eval("Display(CALL_WITH_CATCH(function() return 314; end, []));;");
     test_eval("Display(CALL_WITH_CATCH(function() return [][1]; end, []));;");
     return 0;
