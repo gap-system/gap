d82b1e7fdefde81985cadc5f1733a331abaaef6f
Nikita Hismatov <hismatovns@gmail.com>
2022-07-28 21:29:40 +0500

    no repeating (magic) number 4 in cpu.c
    Introduced a static const UInt FALLBACK_CPUS_NUMBER in cpu.c to use it instead of just 4

--- src/hpc/cpu.c
+++ src/hpc/cpu.c
@@ -16,12 +16,14 @@
 #include <windows.h>
 #endif
 
+static const UInt FALLBACK_CPUS_NUMBER = 4;
+
 /****************************************************************************
 **
 *V  SyNumProcessors  . . . . . . . . . . . . . . . . . number of logical CPUs
 **
 */
-UInt SyNumProcessors = 4;
+UInt SyNumProcessors = FALLBACK_CPUS_NUMBER;
 
 /****************************************************************************
 **
@@ -31,16 +33,15 @@ UInt SyNumProcessors = 4;
 */
 UInt SyCountProcessors(void)
 {
-    const UInt fallback_cpus_number = 4;
 #if _POSIX_C_SOURCE
     const int result = sysconf(_SC_NPROCESSORS_ONLN);
     if (result < 1) {
-        return fallback_cpus_number;
+        return FALLBACK_CPUS_NUMBER;
     }
     return result;
 #elif _WIN32_WINNT >= _WIN32_WINNT_WIN7
     return GetActiveProcessorCount(ALL_PROCESSOR_GROUPS);
 #else
-    return fallback_cpus_number;
+    return FALLBACK_CPUS_NUMBER;
 #endif
 }
