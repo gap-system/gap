gap-system/gap

commit fab1afb8488eae8bc812a858926bd98d8b794010

kernel: fix two warnings reported by clang 10
For the record, the one in gasman.c was:

src/gasman.c:1900:55: error: expression does not compute the number
      of elements in this array; element type is 'int', not 'Bag'
      (aka 'unsigned long **') [-Werror,-Wsizeof-array-div]
          p < (Bag*)((void*)RegsBags)+sizeof(RegsBags)/sizeof(Bag);
                                             ~~~~~~~~ ^

But that code simply wants to skip to the end of RegsBag, which we can do in a
much simper fashion

src/gasman.c
@@ -1895,10 +1895,11 @@ static void GenStackFuncBags(void)
         }
     }
 
-    /* mark from registers, dirty dirty hack                               */
-    for ( p = (Bag*)((void*)RegsBags);
-          p < (Bag*)((void*)RegsBags)+sizeof(RegsBags)/sizeof(Bag);
-          p++ )
+    // mark from registers, dirty dirty hack: we treat the jmp_buf as a
+    // sequence of Bag values. Note that sizeof(jmp_buf) need not be a
+    // multiple of sizeof(Bag), hence the end condition looks slightly.
+    // unusual.
+    for (p = (Bag *)RegsBags; p + 1 <= (Bag *)(RegsBags + 1); p++)
         MarkBag( *p );
 
 #ifdef DEBUG_GASMAN_MARKING

src/plist.c
@@ -3657,7 +3657,7 @@ static Int InitKernel (
       MakeImmutableObjFuncs[ t1 ] = MakeImmutablePlistNoMutElms;
 
     /* mutable tables may have mutable rows */
-      MakeImmutableObjFuncs[T_PLIST_TAB] = MakeImmutablePlistInHom;
+    MakeImmutableObjFuncs[T_PLIST_TAB] = MakeImmutablePlistInHom;
 
 #ifdef HPCGAP
     for ( t1 = T_PLIST; t1 <= LAST_PLIST_TNUM; t1 += 2 ) {

commit 51516a6630e452827c3366ba099e9c4e4530dfcb

Fix GASMAN register marking
Marking of bag pointers stored in registers was broken by me in
commit fab1afb8488eae8bc812a858926bd98d8b794010 -- the register marking
loop ended up iterating zero times. This is now fixed (and verified to
be fixed by the good old "insert printf to see what it marks" technique...

src/gasman.c
@@ -1921,11 +1921,12 @@ static NOINLINE void GenStackFuncBags(void)
         }
     }
 
-    // mark from registers, dirty dirty hack: we treat the jmp_buf as a
-    // sequence of Bag values. Note that sizeof(jmp_buf) need not be a
-    // multiple of sizeof(Bag), hence the end condition looks slightly.
-    // unusual.
-    for (p = (Bag *)RegsBags; p + 1 <= (Bag *)(RegsBags + 1); p++)
+    // mark content of registers, dirty dirty hack: we treat the jmp_buf
+    // as a sequence of Bag values. Note that sizeof(jmp_buf) need not
+    // be a multiple of sizeof(Bag), hence the end condition looks
+    // slightly. unusual.
+    for (p = (Bag *)RegsBags;
+         p < (Bag *)((char *)RegsBags + sizeof(RegsBags)); p++)
         MarkBag( *p );
 
 #ifdef DEBUG_GASMAN_MARKING