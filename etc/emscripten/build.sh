fcaba236103df173ff200fdfe5d719fb8c9f47cd
Chris Jefferson <caj21@st-andrews.ac.uk>
2022-10-30 20:24:44 +0000

    Add 'tst' directory to default emscripten build

--- etc/emscripten/build.sh
+++ etc/emscripten/build.sh
@@ -90,4 +90,4 @@ cp native-build/build/c_*.c native-build/build/ffdata.* src/
 
 # The EXEEXT is usually for windows, but here it lets us set GAP's extension,
 # which lets us produce a html page to run GAP in
-emmake make -j8 LDFLAGS="--preload-file pkg --preload-file lib --preload-file grp -s ASYNCIFY=1 -sTOTAL_STACK=32mb -sASYNCIFY_STACK_SIZE=32000000 -sINITIAL_MEMORY=2048mb -O2" EXEEXT=".html"
+emmake make -j8 LDFLAGS="--preload-file pkg --preload-file lib --preload-file grp --preload-file tst -s ASYNCIFY=1 -sTOTAL_STACK=32mb -sASYNCIFY_STACK_SIZE=32000000 -sINITIAL_MEMORY=2048mb -O2" EXEEXT=".html"
