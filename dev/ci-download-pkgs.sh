58fa08c23eef98d218d66cafb63ef62abad2e317
Max Horn <max@quendi.de>
2023-12-04 21:29:21 +0100

    Restrict SetDimension method to IsFreeLeftModule (#5520)

--- dev/ci-download-pkgs.sh
+++ dev/ci-download-pkgs.sh
@@ -5,6 +5,7 @@ SRCDIR=${SRCDIR:-$PWD}
 
 # change into BUILDDIR
 [[ -n "$BUILDDIR" ]] && cd "$BUILDDIR"
+BUILDDIR=$PWD
 
 # if requested, bootstrap with minimal set of packages
 if [[ $BOOTSTRAP_MINIMAL = yes ]]
@@ -29,3 +30,16 @@ fi
 
 echo "ls $SRCDIR/pkg"
 ls $SRCDIR/pkg
+
+# reset CFLAGS, CXXFLAGS, LDFLAGS before compiling packages, to prevent
+# them from being compiled with coverage gathering, because
+# otherwise gcov may confuse IO's src/io.c, or anupq's src/read.c,
+# with GAP kernel files with the same name
+unset CFLAGS
+unset CXXFLAGS
+unset LDFLAGS
+
+# compile a few packages useful for some tests, e.g. in testbugfix
+pushd "$SRCDIR/pkg"
+"$SRCDIR/bin/BuildPackages.sh" --strict --with-gaproot="$BUILDDIR" io* cvec*
+popd
