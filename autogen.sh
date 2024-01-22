898e45e153196dffc5f3ba0b927713b9d43a275d
Max Horn <max@quendi.de>
2024-01-22 23:33:23 +0100

    buildsys: add some comments

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
 # Regenerate configure from configure.ac. Requires GNU autoconf.
+# We do not use autoreconf because it is only part of GNU automake,
+# and also because it had various other issues in the past.
 set -ex
 autoconf -Wall -f
 autoheader -Wall -f
