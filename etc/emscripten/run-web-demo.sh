cba715770d0fe3a09f5ad51684c5920c78cc781d
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-03-12 18:02:48 +0800

    Update run-web-demo.sh to use filename and path hashing
    Add asset copying and error handling to run-web-demo.sh

--- etc/emscripten/run-web-demo.sh
+++ etc/emscripten/run-web-demo.sh
@@ -4,9 +4,16 @@ set -eux
 
 echo This script assumes you have already run 'build.sh'
 
-mkdir -p web-example
-cp etc/emscripten/web-template/* web-example
-cp gap.js gap.wasm gap.worker.js web-example
-cp -r pkg lib grp tst doc hpcgap dev benchmark web-example/
+mkdir -p web-example/assets
+cp etc/emscripten/web-template/* web-example/
+cp gap.js gap.wasm gap.worker.js web-example/ 
+
+find pkg lib grp tst doc hpcgap dev benchmark -type f | python3 etc/emscripten/copy_hashed_assets.py web-example/assets
+
+if [ $? -ne 0 ]; then
+    echo "Copying failed."
+    exit 1
+fi
+
 cd web-example
 ../etc/emscripten/server.rb
