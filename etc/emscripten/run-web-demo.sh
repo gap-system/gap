6853c5d747a0bf37c9a0137b84143eac392746ba
Yanshu Wang <wangyenshu@outlook.com>
2026-03-31 10:15:39 +0800

    remove double percentage encoding; separate gap-fs.js and gap-fs.json

--- etc/emscripten/run-web-demo.sh
+++ etc/emscripten/run-web-demo.sh
@@ -4,16 +4,10 @@ set -eux
 
 echo This script assumes you have already run 'build.sh'
 
-mkdir -p web-example/assets
+mkdir -p web-example
 cp etc/emscripten/web-template/* web-example/
-cp gap.js gap.wasm gap.worker.js web-example/ 
+cp gap.js gap.wasm gap.worker.js gap-fs.json web-example/ 
 
-find pkg lib grp tst doc hpcgap dev benchmark -type f | python3 etc/emscripten/copy_encoded_assets.py web-example/assets
-
-if [ $? -ne 0 ]; then
-    echo "Copying failed."
-    exit 1
-fi
-
+cp -r pkg lib grp tst doc hpcgap dev benchmark web-example/
 cd web-example
 ../etc/emscripten/server.rb
