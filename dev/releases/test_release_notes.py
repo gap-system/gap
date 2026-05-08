70623635301bb032b2b5985b52a9c5ccd78736b3
Max Horn <max@quendi.de>
2026-05-08 12:24:11 +0100

    Update dev/releases/test_release_notes.py
    Co-authored-by: Reinis Cirpons <43414125+reiniscirpons@users.noreply.github.com>

--- dev/releases/test_release_notes.py
+++ dev/releases/test_release_notes.py
@@ -13,7 +13,6 @@ def load_release_notes_module():
 
 
 def test_is_dependabot_pr_detects_dependabot_author():
-    release_notes = load_release_notes_module()
     pr = {
         "author": {"is_bot": True, "login": "app/dependabot"},
         "labels": [
@@ -23,3 +22,97 @@ def test_is_dependabot_pr_detects_dependabot_author():
     }
 
     assert release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": True, "login": "app/notdependabot"},
+        "labels": [
+            {"name": "dependencies"},
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": False, "login": "app/dependabot"},
+        "labels": [
+            {"name": "dependencies"},
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": False, "login": "app/notdependabot"},
+        "labels": [
+            {"name": "dependencies"},
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert not release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": False, "login": "app/dependabot"},
+        "labels": [
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": False, "login": "app/notdependabot"},
+        "labels": [
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert not release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": True, "login": "app/notdependabot"},
+        "labels": [
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert not release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"is_bot": False, "login": "dependabot[bot]"},
+        "labels": [
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert release_notes.is_dependabot_pr(pr)
+    
+        pr = {
+        "author": {"login": "dependabot[bot]"},
+        "labels": [
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"login": "app/notdependabot"},
+        "labels": [
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert not release_notes.is_dependabot_pr(pr)
+
+    pr = {
+        "author": {"login": "app/notdependabot"},
+        "labels": [
+            {"name": "dependencies"},
+            {"name": "github_actions"},
+        ],
+    }
+
+    assert not release_notes.is_dependabot_pr(pr)
