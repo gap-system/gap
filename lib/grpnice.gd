cb7672c3b274bdd5365d63b38e1a37e18d9b4ab2
Max Horn <max@quendi.de>
2026-07-13 22:27:06 +0200

    Change deprecated RUN_IN_GGMBI to `fail` (#6458)
    This variable is still set by old versions of the `matgrp`
    package.
    
    By setting the default to `fail`, though, newer `matgrp`
    versions can detect that it runs in a GAP version where
    `RUN_IN_GGMBI` is deprecated, and can then instead use the
    `Run_In_GGMBI` global option.
    
    Follow up to PR #6442.

--- lib/grpnice.gd
+++ lib/grpnice.gd
@@ -192,7 +192,7 @@ InstallSubsetMaintenance( IsHandledByNiceMonomorphism,
 # We recommend the (documented) global option `Run_In_GGMBI`
 # instead of the global variable `RUN_IN_GGMBI`,
 # but `RUN_IN_GGMBI` is still supported because GAP packages might use it.
-RUN_IN_GGMBI:=false; # If somebody would call `GHBI' to make a
+RUN_IN_GGMBI:=fail;  # If somebody would call `GHBI' to make a
                      # NiceMonomorphism, we would get an infinite recursion.
                      # This flag can be set to avoid GHBIs to be translated
                      # via the niceo. If it is set, the method which does
